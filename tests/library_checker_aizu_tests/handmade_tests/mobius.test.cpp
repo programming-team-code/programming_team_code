a85521879f6fbc8b0dc6b6e3769f5786f17df38b
Luke Videckis <lukevideckis@gmail.com>
2024-02-21 13:01:49 -0600

    somehow this fixes the warning

--- tests/library_checker_aizu_tests/handmade_tests/mobius.test.cpp
+++ tests/library_checker_aizu_tests/handmade_tests/mobius.test.cpp
@@ -6,6 +6,9 @@
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 #include "../../../library/math/mobius.hpp"
+#include "../../../library/math/prime_sieve/calc_sieve.hpp"
+#include "../../../library/math/prime_sieve/is_prime.hpp"
+	assert(is_prime(2));
 	for (int i = 1; i < ssize(mobius); i++) {
 		int val = i;
 		map<ull, int> factors;
