3b2e0035e9c51ef49368e3e1f4640f6b0e4ac578
Luke Videckis <lukevideckis@gmail.com>
2024-02-21 14:21:39 -0600

    add docs for this

--- library/math/prime_sieve/get_prime_factors.hpp
+++ library/math/prime_sieve/get_prime_factors.hpp
@@ -1,3 +1,9 @@
 /** @file */
 #pragma once
+/**
+ * `fac` will equal all prime factors of val, increasing
+ * @param val positive integer
+ * @time O(log(val))
+ * @space O(1)
+ */
 for (int left = val, fac = sieve[left]; left > 1; fac = sieve[left /= fac])
