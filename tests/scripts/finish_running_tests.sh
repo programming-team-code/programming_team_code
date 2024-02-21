f9aa5e11df2de70eb57bfa132645a0aaf897ebe3
Luke Videckis <lukevideckis@gmail.com>
2024-02-20 18:46:39 -0600

    script to run tests every 15 mins until every test ran

---
+++ tests/scripts/finish_running_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ** glob now searches any number of levels
+shopt -s globstar
+
+# assuming you're on `dev` branch, and in `tests` folder, and
+# have run this via `make finish_running_tests`
+
+echo "pulling dev"
+git pull
+
+while [ ./scripts/print_which_tests_havent_run.sh ];
+do
+	echo "not all tests ran; starting CI to run more tests"
+	gh workflow run 'programming team code CI' --ref dev
+	echo "sleeping for 15 minutes"
+	sleep 15m
+	echo "pulling dev"
+	git pull
+done
