4da0d745ba3cd524100042f124d114cd08d5822a
Luke Videckis <lukevideckis@gmail.com>
2024-02-25 13:25:19 -0600

    golf linear lca
    commit 7ba81ae8038d77ae1128641b8401ce7c94fd8137
    Author: GitHub <noreply@github.com>
    Date:   Sun Feb 25 17:04:22 2024 +0000
    
        [auto-verifier] verify commit c0f77d6dae2f4ed54fdf12c319a357662cbecf1a
    
    commit c0f77d6dae2f4ed54fdf12c319a357662cbecf1a
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Sun Feb 25 10:51:25 2024 -0600
    
        consistent var names
    
    commit a9ebbadf1225acf7516717ee333476925f272879
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Sun Feb 25 10:48:15 2024 -0600
    
        fix dox
    
    commit 4c62fcf2791c41c37f6f1d369832594a59abdec8
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Sun Feb 25 10:47:22 2024 -0600
    
        golf

--- library/data_structures/uncommon/linear_rmq.hpp
+++ library/data_structures/uncommon/linear_rmq.hpp
@@ -11,7 +11,7 @@ template <class T, class F> struct linear_rmq {
 	int n;
 	F cmp;
 	vector<T> a;
-	vector<int> par_head;
+	vector<int> head;
 	vector<unsigned> in_label, ascendant;
 	/**
 	 * @code{.cpp}
@@ -25,13 +25,13 @@ template <class T, class F> struct linear_rmq {
 	 * @time O(n)
 	 * @space O(n)
 	 */
-	linear_rmq(const vector<T>& a_a, F a_cmp) : n(ssize(a_a)), cmp(a_cmp), a(a_a), par_head(n + 1), in_label(n), ascendant(n) {
+	linear_rmq(const vector<T>& a_a, F a_cmp) : n(ssize(a_a)), cmp(a_cmp), a(a_a), head(n + 1), in_label(n), ascendant(n) {
 		vector<int> ri(mono_st(a, cmp)), le(mono_range(ri)), p(cart_binary_tree(ri));
 		for (int i = 0; i < n; i++)
 			in_label[i] = ri[i] & -bit_floor(unsigned((le[i] + 1) ^ ri[i]));
 		for (int i = 0; i < n; i++)
 			if (p[i] == n || in_label[p[i]] != in_label[i]) {
-				par_head[in_label[i]] = p[i];
+				head[in_label[i]] = p[i];
 				int to_add = in_label[i] & -in_label[i];
 				ascendant[le[i] + 1] += to_add;
 				if (ri[i] < n) ascendant[ri[i]] -= to_add;
@@ -40,7 +40,7 @@ template <class T, class F> struct linear_rmq {
 	}
 	inline int lift(int u, unsigned j) {
 		auto k = bit_floor(ascendant[u] ^ j);
-		return k == 0 ? u : par_head[(in_label[u] & -k) | k];
+		return k == 0 ? u : head[(in_label[u] & -k) | k];
 	}
 	/**
 	 * @param le,ri defines range [min(le, ri), max(le, ri)]
--- library/graphs/linear_lca/linear_lca.hpp
+++ library/graphs/linear_lca/linear_lca.hpp
@@ -7,46 +7,37 @@ inline int bit_floor(unsigned x) {return x ? 1 << __lg(x) : 0;}
  */
 struct linear_lca {
 	vector<vector<int>> adj;
-	int n, timer = 1;
-	vector<int> d, par_head;
+	int n;
+	vector<int> d, head, order;
 	vector<unsigned> in_label, ascendant;
 	/**
 	 * @param a_adj forest (rooted or unrooted)
 	 * @time O(n)
 	 * @space O(n)
 	 */
-	linear_lca(const vector<vector<int>>& a_adj) : adj(a_adj), n(size(adj)), d(n), par_head(n + 1), in_label(n), ascendant(n) {
+	linear_lca(const vector<vector<int>>& a_adj) : adj(a_adj), n(size(adj)), d(n), head(n + 1), in_label(n), ascendant(n) {
 		for (int i = 0; i < n; i++)
-			if (in_label[i] == 0) {
-				dfs1(i, -1);
-				ascendant[i] = in_label[i];
-				dfs2(i, -1);
-			}
+			if (in_label[i] == 0) dfs(i, i);
+		for (int u : order) {
+			int x = in_label[u];
+			ascendant[u] = (x & -x) | ascendant[head[x]];
+		}
 	}
-	void dfs1(int u, int p) {
-		in_label[u] = timer++;
+	void dfs(int u, int p) {
+		order.push_back(u);
+		in_label[u] = ssize(order);
 		for (int v : adj[u])
 			if (v != p) {
 				d[v] = 1 + d[u];
-				dfs1(v, u);
+				dfs(v, u);
 				if (__builtin_ctz(in_label[v]) > __builtin_ctz(in_label[u]))
 					in_label[u] = in_label[v];
 			}
-	}
-	void dfs2(int u, int p) {
-		for (int v : adj[u])
-			if (v != p) {
-				ascendant[v] = ascendant[u];
-				if (in_label[v] != in_label[u]) {
-					par_head[in_label[v]] = u;
-					ascendant[v] += in_label[v] & -in_label[v];
-				}
-				dfs2(v, u);
-			}
+		head[in_label[u]] = p;
 	}
 	inline int lift(int u, unsigned j) {
 		auto k = bit_floor(ascendant[u] ^ j);
-		return k == 0 ? u : par_head[(in_label[u] & -k) | k];
+		return k == 0 ? u : head[(in_label[u] & -k) | k];
 	}
 	/**
 	 * @param u,v nodes
