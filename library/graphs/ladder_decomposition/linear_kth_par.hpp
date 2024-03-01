4ce5890d79a0c105a4db511d28c070fc4a5aebce
Luke Videckis <lukevideckis@gmail.com>
2024-03-01 13:02:27 -0600

    fix clang casing

--- library/graphs/ladder_decomposition/linear_kth_par.hpp
+++ library/graphs/ladder_decomposition/linear_kth_par.hpp
@@ -4,7 +4,7 @@ inline int bit_floor(unsigned x) {return x ? 1 << __lg(x) : 0;}
 /**
  * @see Still Simpler Static Level Ancestors by Torben Hagerup, May 2020; https://codeforces.com/blog/entry/126580
  */
-template <int x = 3> struct linear_kth_par {
+template <int X = 3> struct linear_kth_par {
 	int n;
 	vector<int> d, p, j, dl/*deepest leaf*/, idx;
 	vector<vector<int>> l_tbl;
@@ -14,15 +14,15 @@ template <int x = 3> struct linear_kth_par {
 		   linear_kth_par<5> kp(adj);
 	 * @endcode
 	 * @param adj forest (rooted or unrooted)
-	 * @time O(n * (2 * x + 1) / (x - 1))
-	 * @space O(n * (2 * x + 1) / (x - 1))
+	 * @time O(n * (2 * X + 1) / (X - 1))
+	 * @space O(n * (2 * X + 1) / (X - 1))
 	 */
 	linear_kth_par(const vector<vector<int>>& adj) : n(ssize(adj)), d(n), p(n, -1), dl(n), idx(n), l_tbl(n) {
-		static_assert(x >= 2);
+		static_assert(X >= 2);
 		vector<int> st;
 		auto add_j = [&]() -> void {
 			j.push_back(st[0]);
-			int i = ssize(st) - 1 - (x - 1) * (ssize(j) & -ssize(j));
+			int i = ssize(st) - 1 - (X - 1) * (ssize(j) & -ssize(j));
 			if (i > 0) j.back() = st[i];
 		};
 		auto dfs = [&](auto&& self, int u) -> void {
@@ -45,7 +45,7 @@ template <int x = 3> struct linear_kth_par {
 			if (p[i] == i || dl[p[i]] != dl[i]) {
 				int leaf = dl[i];
 				auto& lad = l_tbl[leaf];
-				lad.resize(min((d[leaf] - d[i]) * (2 * x + 1) / (x - 1), d[leaf] + 1), leaf);
+				lad.resize(min((d[leaf] - d[i]) * (2 * X + 1) / (X - 1), d[leaf] + 1), leaf);
 				for (int k = 1; k < ssize(lad); k++)
 					lad[k] = p[lad[k - 1]];
 			}
@@ -54,16 +54,16 @@ template <int x = 3> struct linear_kth_par {
 	 * @param u query node
 	 * @param k number of edges
 	 * @returns a node k edges up from u
-	 * @time O(x)
-	 * @space O(x)
+	 * @time O(X)
+	 * @space O(X)
 	 */
 	inline int kth_par(int u, int k) {
 		assert(0 <= k && k <= d[u]);
-		if (k < x) {
+		if (k < X) {
 			while (k--) u = p[u];
 			return u;
 		}
-		int i = bit_floor(unsigned(k / x)), leaf = dl[j[((idx[u] & -i) | i) - 1]];
+		int i = bit_floor(unsigned(k / X)), leaf = dl[j[((idx[u] & -i) | i) - 1]];
 		return l_tbl[leaf][k + d[leaf] - d[u]];
 	}
 };
