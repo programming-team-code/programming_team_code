1467f45ea9afe7498163ac7ae97706777508085c
Luke Videckis <lukevideckis@gmail.com>
2024-03-01 12:54:50 -0600

    generalize linear kth par
    commit 0d059b3c1f0494c11cd6c090391ef46642962749
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Fri Mar 1 12:53:09 2024 -0600
    
        update bounds
    
    commit 066ac80832d882661b084e50ea1a879d1942f9c8
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Fri Mar 1 12:45:00 2024 -0600
    
        format
    
    commit cac554a7db27d93bdb03df26e281dfb860c62f0e
    Merge: 63e2ac7 60616fa
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Fri Mar 1 12:44:37 2024 -0600
    
        Merge branch 'dev' into generalize
    
    commit 63e2ac7f90a2974b0e3e112425ca3b940492366c
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Fri Mar 1 12:44:08 2024 -0600
    
        generalize alg

--- library/graphs/ladder_decomposition/linear_kth_par.hpp
+++ library/graphs/ladder_decomposition/linear_kth_par.hpp
@@ -2,22 +2,27 @@
 #pragma once
 inline int bit_floor(unsigned x) {return x ? 1 << __lg(x) : 0;}
 /**
- * @see Still Simpler Static Level Ancestors by Torben Hagerup, May 2020
+ * @see Still Simpler Static Level Ancestors by Torben Hagerup, May 2020; https://codeforces.com/blog/entry/126580
  */
-struct linear_kth_par {
+template <int x = 3> struct linear_kth_par {
 	int n;
 	vector<int> d, p, j, dl/*deepest leaf*/, idx;
 	vector<vector<int>> l_tbl;
 	/**
+	 * @code{.cpp}
+		   linear_kth_par kp(adj);
+		   linear_kth_par<5> kp(adj);
+	 * @endcode
 	 * @param adj forest (rooted or unrooted)
-	 * @time O(n)
-	 * @space O(n)
+	 * @time O(n * (2 * x + 1) / (x - 1))
+	 * @space O(n * (2 * x + 1) / (x - 1))
 	 */
 	linear_kth_par(const vector<vector<int>>& adj) : n(ssize(adj)), d(n), p(n, -1), dl(n), idx(n), l_tbl(n) {
+		static_assert(x >= 2);
 		vector<int> st;
 		auto add_j = [&]() -> void {
 			j.push_back(st[0]);
-			int i = ssize(st) - 1 - 2 * (ssize(j) & -ssize(j));
+			int i = ssize(st) - 1 - (x - 1) * (ssize(j) & -ssize(j));
 			if (i > 0) j.back() = st[i];
 		};
 		auto dfs = [&](auto&& self, int u) -> void {
@@ -40,7 +45,7 @@ struct linear_kth_par {
 			if (p[i] == i || dl[p[i]] != dl[i]) {
 				int leaf = dl[i];
 				auto& lad = l_tbl[leaf];
-				lad.resize(min((d[leaf] - d[i]) * 7 / 2, d[leaf] + 1), leaf);
+				lad.resize(min((d[leaf] - d[i]) * (2 * x + 1) / (x - 1), d[leaf] + 1), leaf);
 				for (int k = 1; k < ssize(lad); k++)
 					lad[k] = p[lad[k - 1]];
 			}
@@ -49,15 +54,16 @@ struct linear_kth_par {
 	 * @param u query node
 	 * @param k number of edges
 	 * @returns a node k edges up from u
-	 * @time O(1)
-	 * @space O(1)
+	 * @time O(x)
+	 * @space O(x)
 	 */
 	inline int kth_par(int u, int k) {
 		assert(0 <= k && k <= d[u]);
-		if (k == 0) return u;
-		if (k == 1) return p[u];
-		if (k == 2) return p[p[u]];
-		int i = bit_floor(unsigned(k / 3)), leaf = dl[j[((idx[u] & -i) | i) - 1]];
+		if (k < x) {
+			while (k--) u = p[u];
+			return u;
+		}
+		int i = bit_floor(unsigned(k / x)), leaf = dl[j[((idx[u] & -i) | i) - 1]];
 		return l_tbl[leaf][k + d[leaf] - d[u]];
 	}
 };
--- tests/library_checker_aizu_tests/graphs/ladder_decomposition.test.cpp
+++ tests/library_checker_aizu_tests/graphs/ladder_decomposition.test.cpp
@@ -16,7 +16,17 @@ int main() {
 		adj[v].push_back(u);
 	}
 	ladder ld(adj);
-	linear_kth_par lin_ld(adj);
+	linear_kth_par<2> lin_ld_2(adj);
+	linear_kth_par<3> lin_ld_3(adj);
+	linear_kth_par<4> lin_ld_4(adj);
+	{
+		array<int, 3> sum_ladder_lengths = {0, 0, 0};
+		for (auto& lad : lin_ld_2.l_tbl) sum_ladder_lengths[0] += ssize(lad);
+		for (auto& lad : lin_ld_3.l_tbl) sum_ladder_lengths[1] += ssize(lad);
+		for (auto& lad : lin_ld_4.l_tbl) sum_ladder_lengths[2] += ssize(lad);
+		for (int kappa = 2; kappa < 5; kappa++)
+			assert(sum_ladder_lengths[kappa - 2] <= n * (2 * kappa + 1) / (kappa - 1));
+	}
 	vector<vector<int>> adj_rooted(n + n);
 	for (int i = 0; i < n; i++)
 		if (ld.p[i] != i) {
@@ -39,8 +49,12 @@ int main() {
 			assert(res == curr_1);
 			auto curr_2 = ld_rooted.kth_par(u + n, k);
 			assert(res == curr_2 - n);
-			auto curr_3 = lin_ld.kth_par(u, k);
-			assert(res == curr_3);
+			auto curr_3_2 = lin_ld_2.kth_par(u, k);
+			assert(res == curr_3_2);
+			auto curr_3_3 = lin_ld_3.kth_par(u, k);
+			assert(res == curr_3_3);
+			auto curr_3_4 = lin_ld_4.kth_par(u, k);
+			assert(res == curr_3_4);
 			auto curr_4 = lin_ld_rooted.kth_par(u, k);
 			assert(res == curr_4);
 			auto curr_5 = lin_ld_rooted.kth_par(u + n, k);
@@ -53,8 +67,12 @@ int main() {
 			assert(res == curr_1);
 			auto curr_2 = ld_rooted.kth_par(v + n, u_lca + v_lca - k);
 			assert(res == curr_2 - n);
-			auto curr_3 = lin_ld.kth_par(v, u_lca + v_lca - k);
-			assert(res == curr_3);
+			auto curr_3_2 = lin_ld_2.kth_par(v, u_lca + v_lca - k);
+			assert(res == curr_3_2);
+			auto curr_3_3 = lin_ld_3.kth_par(v, u_lca + v_lca - k);
+			assert(res == curr_3_3);
+			auto curr_3_4 = lin_ld_4.kth_par(v, u_lca + v_lca - k);
+			assert(res == curr_3_4);
 			auto curr_4 = lin_ld_rooted.kth_par(v, u_lca + v_lca - k);
 			assert(res == curr_4);
 			auto curr_5 = lin_ld_rooted.kth_par(v + n, u_lca + v_lca - k);
