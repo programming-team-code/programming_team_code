c01fb19a6136450992bcade497ce4615f5220141
Luke Videckis <lukevideckis@gmail.com>
2024-03-06 09:41:48 -0700

    format

--- tests/library_checker_aizu_tests/graphs/iterate_subtree.test.cpp
+++ tests/library_checker_aizu_tests/graphs/iterate_subtree.test.cpp
@@ -31,26 +31,26 @@ int main() {
 					continue;
 				}
 #include "../../../library/graphs/lca_rmq/iterate_subtree.hpp"
-					assert(lca.d[u] <= lca.d[v]);
-					assert(in_subtree(u, v));
-					cnt_small_iterated[v]++;
-				}
+				assert(lca.d[u] <= lca.d[v]);
+				assert(in_subtree(u, v));
+				cnt_small_iterated[v]++;
 			}
 		}
-		partial_sum(begin(cnt_big_iterated), end(cnt_big_iterated), begin(cnt_big_iterated));
-		for (int i = 0; i < n; i++) {
-			//at each merge (small to large), subtree size doubles or more
-			//how many doubles until it reaches n?
-			//sub_sz * 2^k ~= n
-			//2^k ~= n / sub_sz
-			//k ~= __lg(n / sub_sz)
-			assert(cnt_small_iterated[i] <= __lg(n / lca.sub_sz[i]));
-			assert(cnt_small_iterated[i] + cnt_big_iterated[lca.in[i]] == lca.d[i]);
-		}
 	}
-	while (q--) {
-		int u, v;
-		cin >> u >> v;
-		cout << lca.get_lca(u, v) << '\n';
+	partial_sum(begin(cnt_big_iterated), end(cnt_big_iterated), begin(cnt_big_iterated));
+	for (int i = 0; i < n; i++) {
+		//at each merge (small to large), subtree size doubles or more
+		//how many doubles until it reaches n?
+		//sub_sz * 2^k ~= n
+		//2^k ~= n / sub_sz
+		//k ~= __lg(n / sub_sz)
+		assert(cnt_small_iterated[i] <= __lg(n / lca.sub_sz[i]));
+		assert(cnt_small_iterated[i] + cnt_big_iterated[lca.in[i]] == lca.d[i]);
 	}
+}
+while (q--) {
+	int u, v;
+	cin >> u >> v;
+	cout << lca.get_lca(u, v) << '\n';
+}
 }
