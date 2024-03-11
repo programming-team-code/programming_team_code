39ecbbaa1133771a5bef2cbb896fd7782247fb65
Luke Videckis <lukevideckis@gmail.com>
2024-03-11 16:02:17 -0600

    less testing

--- tests/library_checker_aizu_tests/compress_tree_asserts.hpp
+++ tests/library_checker_aizu_tests/compress_tree_asserts.hpp
@@ -13,27 +13,25 @@ void compress_tree_asserts(vector<vector<int>> adj, linear_lca& lin_lca) {
 		auto [parent, to_node] = lin_lca.compress_tree({});
 		assert(empty(parent) && empty(to_node));
 	}
-	for (int tests = 0; tests < 2; tests++) {
-		vector<int> subset;
-		{
-			int subset_size = get_rand(1, n);
-			while (subset_size--) {
-				int u = get_rand(0, n - 1);
-				if (!used[u]) {
-					used[u] = 1;
-					subset.push_back(u);
-				}
+	vector<int> subset;
+	{
+		int subset_size = get_rand(1, n);
+		while (subset_size--) {
+			int u = get_rand(0, n - 1);
+			if (!used[u]) {
+				used[u] = 1;
+				subset.push_back(u);
 			}
 		}
-		auto kactl_res = compressTree(kactl_lca, subset);
-		auto [parent, to_node] = lin_lca.compress_tree(subset);
-		assert(ssize(parent) == ssize(to_node) && ssize(parent) == ssize(kactl_res));
-		for (int i = 0; i < ssize(parent); i++) {
-			assert(to_node[i] == kactl_res[i].second);
-			assert(parent[i] < i);
-			if (i > 0) assert(parent[i] == kactl_res[i].first);
-			else assert(parent[i] == -1);
-		}
-		for (int u : subset) used[u] = 0;
 	}
+	auto kactl_res = compressTree(kactl_lca, subset);
+	auto [parent, to_node] = lin_lca.compress_tree(subset);
+	assert(ssize(parent) == ssize(to_node) && ssize(parent) == ssize(kactl_res));
+	for (int i = 0; i < ssize(parent); i++) {
+		assert(to_node[i] == kactl_res[i].second);
+		assert(parent[i] < i);
+		if (i > 0) assert(parent[i] == kactl_res[i].first);
+		else assert(parent[i] == -1);
+	}
+	for (int u : subset) used[u] = 0;
 }
