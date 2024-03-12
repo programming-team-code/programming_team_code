9c6f74c27128e5286d83149fb118aecd42f917d8
cameron custer <custer.cameron@gmail.com>
2024-03-12 19:57:20 -0400

    use multiple templates to support wide & long matricies

--- library/math/matrix_related/binary_matrix_mult.hpp
+++ library/math/matrix_related/binary_matrix_mult.hpp
@@ -5,17 +5,17 @@
  *
  * @param a,b binary matrices
  * @returns a*b mod 2
- * @time O(n * m * inner / w) where w is the machine word size (usually 32 or
+ * @time O(n * m * k / w) where w is the machine word size (usually 32 or
  * 64)
  * @space besides the O(n * inner) and O(inner * m) params, this function
  * allocates and returns a O(n * m) vector
  */
-template <unsigned long int N>
-array<bitset<N>, N> operator*(const array<bitset<N>, N>& a,
-                              const array<bitset<N>, N>& b) {
-	array<bitset<N>, N> c;
-	for (auto i = 0; i < N; i++)
-		for (auto j = 0; j < N; j++)
+template <unsigned long int N, unsigned long int M, unsigned long int K>
+array<bitset<K>, N> operator*(const array<bitset<M>, N>& a,
+                              const array<bitset<K>, M>& b) {
+	array<bitset<K>, N> c;
+	for (unsigned long int i = 0; i < N; i++)
+		for (unsigned long int j = 0; j < M; j++)
 			if (a[i][j]) c[i] ^= b[j];
 	return c;
 }
