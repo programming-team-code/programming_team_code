ff74bc4c70be798e429f23e5599b89c284afaf43
Cameron Custer <73217097+cameroncuster@users.noreply.github.com>
2024-03-12 21:13:32 -0400

    binary matrix mult (#6)
    * add binary matrix mult
    
    * [auto-verifier] verify commit e4fcd6278e29d0218bbcd64d8b91e853e2c65497
    
    * use multiple templates to support wide & long matricies
    
    * [auto-verifier] verify commit f3606aaefb677037eb10bf31cc6d45b3f73a85fb
    
    * update docs for binary matrix mult
    
    * [auto-verifier] verify commit 9633d70130102632471ac4d88ecad9319be74ef4
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ library/math/matrix_related/binary_matrix_mult.hpp
@@ -0,0 +1,21 @@
+/** @file */
+#pragma once
+/**
+ * @see https://codeforces.com/blog/entry/80195
+ *
+ * @param a,b binary matrices
+ * @returns a*b mod 2
+ * @time O(n * m * k / w) where w is the machine word size (usually 32 or
+ * 64)
+ * @space O(N * K / w + N * M / w + M * K / w) where w is the machine word size
+ * (usually 32 or 64)
+ */
+template <unsigned long int N, unsigned long int M, unsigned long int K>
+array<bitset<K>, N> operator*(const array<bitset<M>, N>& a,
+                              const array<bitset<K>, M>& b) {
+	array<bitset<K>, N> c;
+	for (unsigned long int i = 0; i < N; i++)
+		for (unsigned long int j = 0; j < M; j++)
+			if (a[i][j]) c[i] ^= b[j];
+	return c;
+}
