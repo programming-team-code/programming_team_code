1f46eb1df40b19488fd3369896b3165313773d1d
Luke Videckis <lukevideckis@gmail.com>
2024-04-01 22:21:30 -0400

    fix sign compare

--- library/math/matrix_related/binary_matrix_mult.hpp
+++ library/math/matrix_related/binary_matrix_mult.hpp
@@ -14,8 +14,8 @@ template <unsigned long int N, unsigned long int M, unsigned long int K>
 array<bitset<K>, N> operator*(const array<bitset<M>, N>& a,
                               const array<bitset<K>, M>& b) {
   array<bitset<K>, N> c;
-  rep (i, 0, N)
-    rep (j, 0, M)
+  rep (i, 0, int(N))
+    rep (j, 0, int(M))
       if (a[i][j]) c[i] ^= b[j];
   return c;
 }
