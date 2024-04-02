8314128de9c271e564c3a0630849a7c39f04e228
Luke Videckis <lukevideckis@gmail.com>
2024-04-01 20:50:59 -0400

    adding only one change to test CI

--- library/flow/min_cost_max_flow.hpp
+++ library/flow/min_cost_max_flow.hpp
@@ -45,7 +45,7 @@ struct mcmf {
         int u = q[qh++];
         id[u] = 2;
         if (qh == n) qh = 0;
-        for (int i = 0; i < sz(adj[u]); i++) {
+        rep(i,0,sz(adj[u])) {
           edge& r = e[adj[u][i]];
           if (r.flow < r.cap && d[u] + r.cost < d[r.v]) {
             d[r.v] = d[u] + r.cost;
