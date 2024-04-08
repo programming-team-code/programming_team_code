b4f7c55b8644e1794943e77c35b860508ff4899d
Cameron Custer <73217097+cameroncuster@users.noreply.github.com>
2024-04-08 09:59:28 -0400

    add xor convolution (#10)
    * add xor convolution
    
    * [auto-verifier] verify commit 2cae7d99551ef8d66032e726cc57a0a8ecf961cd
    
    * clean-up xor_convolution
    
    * [auto-verifier] verify commit f066acb6caddaa89d82fdd9cf47bc42bbb169b97
    
    * kactl macros :face_vomiting:
    
    * [auto-verifier] verify commit b981933e519827c882edab8706d919893b62e267
    
    * more macros...
    
    * [auto-verifier] verify commit aab48920c0565409d0cbfc49c6477510dbcc37cd
    
    * add note about RTE when arrays are empty
    
    * [auto-verifier] verify commit 6681b952d5d3a0c145828a02fd6163106bb486e2
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ tests/library_checker_aizu_tests/convolution/xor_convolution.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/bitwise_xor_convolution"
+#include "../template.hpp"
+
+#include "../../../library/convolution/xor_convolution.hpp"
+
+istream& operator>>(istream& is, vector<int>& v) {
+  for (auto& x : v) is >> x;
+  return is;
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int n;
+  cin >> n;
+  vector<int> a(1 << n), b(1 << n);
+  cin >> a >> b;
+  auto c = xor_convolution(a, b);
+  for (int i = 0; i < (1 << n); i++) cout << c[i] << ' ';
+  cout << '\n';
+  return 0;
+}
