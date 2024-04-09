1eafbaf9e98ffaf9905a1b87b27aa14d4ff86048
Luke Videckis <lukevideckis@gmail.com>
2024-04-09 15:16:08 -0400

    add kactl with no macros

--- tests/scripts/update_main.sh
+++ tests/scripts/update_main.sh
@@ -11,9 +11,6 @@ git submodule update
 
 echo "DON'T PUSH ANY OF THESE CHANGES TO THE REPO!!!!!!!!"
 
-# in order to expand the kactl macros, you need to copy-paste the macros into
-# the beginning of each .hpp file
-sed --in-place "1r library_checker_aizu_tests/kactl_macros.hpp" ../library/**/*.hpp
 # remove `/** @file */` comments
 sed --in-place '/^\/\*\* @file \*\/$/d' ../library/**/*.hpp
 # remove NOLINTNEXTLINE comments
@@ -22,12 +19,18 @@ sed --in-place '/^\/\/ NOLINTNEXTLINE(readability-identifier-naming)$/d' ../libr
 # to copy files like template.cpp, *.sh, *.png, *.md to root directory; so they exist in main branch
 # also to create all directories (cpp command below fails if destination path isn't created)
 cp -r ../library/. ../
+cp -r ../kactl/content/. ../kactl_expanded_macros/
 
+# in order to expand the kactl macros, you need to copy-paste the macros into
+# the beginning of each .hpp file
 for header in ../library/**/*.hpp; do
-	cpp -std=c17 -nostdinc -C -P "$header" "${header/\/library/}"
+  cpp -std=c17 -nostdinc -C -P <(cat library_checker_aizu_tests/kactl_macros.hpp "$header") "${header/\/library/}"
+done
+for header in ../kactl/content/**/*.h; do
+  cpp -std=c17 -nostdinc -C -P <(cat library_checker_aizu_tests/kactl_macros.hpp "$header") "${header/kactl\/content/kactl_expanded_macros}"
 done
 
 # the cpp preprocessor sometimes leaves blank empty lines
-sed --in-place '/^[[:space:]]*$/d' ../**/*.hpp
+sed --in-place '/^[[:space:]]*$/d' ../**/*.hpp ../**/*.h
 # cpp command messes up formatting
 make do_format
