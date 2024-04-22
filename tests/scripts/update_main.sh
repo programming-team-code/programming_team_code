1b0cb94b9aefeb40921dd4ad9d65169e6823dad2
Luke Videckis <lukevideckis@gmail.com>
2024-04-22 14:24:06 -0600

    revert

--- tests/scripts/update_main.sh
+++ tests/scripts/update_main.sh
@@ -22,18 +22,12 @@ sed --in-place '/^\/\/ NOLINTNEXTLINE(readability-identifier-naming)$/d' ../libr
 # to copy files like template.cpp, *.sh, *.png, *.md to root directory; so they exist in main branch
 # also to create all directories (cpp command below fails if destination path isn't created)
 cp -r ../library/. ../
-cp -r ../kactl/content/. ../kactl_expanded_macros/
 
-# in order to expand the kactl macros, you need to copy-paste the macros into
-# the beginning of each .hpp file
 for header in ../library/**/*.hpp; do
 	cpp -std=c17 -nostdinc -C -P "$header" "${header/\/library/}"
 done
-for header in ../kactl/content/**/*.h; do
-	cpp -std=c17 -nostdinc -C -P <(cat library_checker_aizu_tests/kactl_macros.hpp "$header") "${header/kactl\/content/kactl_expanded_macros}"
-done
 
 # the cpp preprocessor sometimes leaves blank empty lines
-sed --in-place '/^[[:space:]]*$/d' ../**/*.hpp ../**/*.h
+sed --in-place '/^[[:space:]]*$/d' ../**/*.hpp
 # cpp command messes up formatting
 make do_format
