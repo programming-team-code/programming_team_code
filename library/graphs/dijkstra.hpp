6b65c74132ed9773c8489d742fb8295b6325f9fc
Luke Videckis <lukevideckis@gmail.com>
2024-05-01 16:51:04 -0600

    better complexity bounds for dijkstra

--- library/graphs/dijkstra.hpp
+++ library/graphs/dijkstra.hpp
@@ -2,23 +2,21 @@
 #pragma once
 /**
  * @param adj directed or undirected, weighted graph
- * @param s a node
- * @returns array `d` where `d[i]` = shortest path from node `s` to node
- * `i`. `d[s]` is always 0.
- * @time O((n + m) log n) Note log(m) < log(n^2) = 2*log(n), so O(log n) ==
- * O(log m)
- * @space besides the O(n + m) param `adj`, this function allocates/returns a
- * O(n) distance array. The (temporary) priority queue can grow to size O(n+m)
+ * @param s start node
+ * @returns vector d where d[i] = length of shortest path from s to i
+ * @time O(n + (m log m))
+ * @space besides the O(n + m) param adj, this function allocates/returns a
+ * O(n) distance array. The (temporary) priority queue can grow to size O(m)
  */
 vector<ll> dijkstra(const vector<vector<pair<int, ll>>>& adj, int s) {
-  using node = pair<ll, int>;
-  vector<ll> d(sz(adj), LLONG_MAX);
-  priority_queue<node, vector<node>, greater<node>> pq;
+  using dt = pair<ll, int>;
+  priority_queue<dt, vector<dt>, greater<dt>> pq;
   pq.emplace(0, s);
+  vector<ll> d(sz(adj), LLONG_MAX);
   while (!empty(pq)) {
     auto [d_u, u] = pq.top();
     pq.pop();
-    if (d[u] <= d_u) continue;  // important check: O(n*m) without it
+    if (d[u] <= d_u) continue;
     d[u] = d_u;
     for (auto [v, w] : adj[u]) pq.emplace(w + d[u], v);
   }
