c48a237558af31df17fd7f190c7077ea76954a18
Cameron Custer <73217097+cameroncuster@users.noreply.github.com>
2024-05-29 12:00:50 -0400

    add comp graph ccs (#15)
    * add comp graph ccs
    
    * fix doxygen
    
    * fix bounds error
    
    * [auto-verifier] verify commit fbf7f83c08692c3145dd60c7267c15257987c762
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ tests/library_checker_aizu_tests/graphs/connected_components_of_complement_graph.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/connected_components_of_complement_graph"
+
+#include "../template.hpp"
+#include "../../../library/graphs/complement_graph_ccs.hpp"
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int>> adj(n);
+  for (int i = 0; i < m; i++) {
+    int u, v;
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  auto cc_id = get_complement_graph_ccs(adj);
+  int num_ccs = *max_element(begin(cc_id), end(cc_id)) + 1;
+  vector<vector<int>> ccs(num_ccs);
+  for (int u = 0; u < n; u++) {
+    ccs[cc_id[u]].push_back(u);
+  }
+  cout << num_ccs << '\n';
+  for (auto& cc : ccs) {
+    cout << size(cc);
+    for (int v : cc) {
+      cout << ' ' << v;
+    }
+    cout << '\n';
+  }
+  return 0;
+}
