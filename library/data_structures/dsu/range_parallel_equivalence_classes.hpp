55d3abce51677f8bec930e38db6c0e93f0d08d17
Cameron Custer <custer.cameron@gmail.com>
2024-06-13 10:57:23 -0600

    fix variable names

--- library/data_structures/dsu/range_parallel_equivalence_classes.hpp
+++ library/data_structures/dsu/range_parallel_equivalence_classes.hpp
@@ -6,7 +6,7 @@
  * to join the ranges [l1, l1 + len) and [l2, l2 + len) such that edges of the
  * form (l1 + i, l2 + i) are joined for all `i` in [0, len).
  *
- * @param rest the vector of restrictions of the form (l1, l2, len)
+ * @param rests the vector of restrictions of the form (l1, l2, len)
  * @param n the number of elements
  *
  * @returns a dsu satisfying the equivalences classes
@@ -14,13 +14,13 @@
  * @time O((n + q) inverse ack n) amortized across all queries
  * @space O(n + q)
  */
-UF get_range_parallel_equivalence_classes(const vector<array<int, 3>>& rest,
+UF get_range_parallel_equivalence_classes(const vector<array<int, 3>>& rests,
                                           int n) {
-  vector<vector<array<int, 2>>> queries_by_len(n + 1);
-  for (auto [l1, l2, len] : queries) queries_by_len[len].push_back({l1, l2});
+  vector<vector<array<int, 2>>> rests_by_len(n + 1);
+  for (auto [l1, l2, len] : rests) rests_by_len[len].push_back({l1, l2});
   UF uf(n);
   for (int len = n; len > 0; len--)
-    for (auto [l1, l2] : queries_by_len[len])
-      if (uf.join(l1, l2)) queries_by_len[len - 1].push_back({l1 + 1, l2 + 1});
+    for (auto [l1, l2] : rests_by_len[len])
+      if (uf.join(l1, l2)) rests_by_len[len - 1].push_back({l1 + 1, l2 + 1});
   return uf;
 }
