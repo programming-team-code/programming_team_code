5ba7070238eb917566ae54a6c3d86a07b1d63757
Cameron Custer <custer.cameron@gmail.com>
2024-06-28 20:09:44 -0600

    validate runtime of incremental offline scc

--- tests/library_checker_aizu_tests/scc_asserts.hpp
+++ tests/library_checker_aizu_tests/scc_asserts.hpp
@@ -1,5 +1,7 @@
 #pragma once
+#include "../../library/contest/random.hpp"
 #include "../../library/graphs/strongly_connected_components/add_edges_strongly_connected.hpp"
+#include "../../library/graphs/strongly_connected_components/offline_incremental_scc.hpp"
 void scc_asserts(const vector<vector<int>>& adj) {
   int n = sz(adj);
   auto [num_sccs, scc_id] = sccs(adj);
@@ -8,28 +10,49 @@ void scc_asserts(const vector<vector<int>>& adj) {
     for (int i = 0; i < n; i++)
       for (auto j : adj[i]) assert(scc_id[i] >= scc_id[j]);
   }
-  vector<bool> is_zero_in(num_sccs, 1), is_zero_out(num_sccs, 1);
-  for (int i = 0; i < n; i++) {
-    for (int v : adj[i]) {
-      if (scc_id[i] == scc_id[v]) continue;
-      is_zero_in[scc_id[v]] = 0;
-      is_zero_out[scc_id[i]] = 0;
+  {
+    // tests offline_incremental_scc
+    vector<array<int, 2>> eds;
+    for (int i = 0; i < n; i++)
+      for (int j : adj[i])
+        if (i != j) eds.push_back({i, j});
+    shuffle(all(eds), rng);
+    auto joins = offline_incremental_scc(eds, n);
+    int m = sz(eds);
+    for (int t = 0; t < m; t++) {
+      auto [u, v] = eds[t];
+      if (scc_id[u] == scc_id[v]) {
+        assert(joins[t] < m);
+      } else {
+        assert(joins[t] == m);
+      }
     }
   }
-  // since {num_sccs-1, ..., 2, 1, 0} is a topo order
-  assert(is_zero_in[num_sccs - 1] && is_zero_out[0]);
-  int num_zero_in = int(count(begin(is_zero_in), end(is_zero_in), 1));
-  int num_zero_out = int(count(begin(is_zero_out), end(is_zero_out), 1));
-  vector<pair<int, int>> edges = extra_edges(adj, num_sccs, scc_id);
-  if (num_sccs == 1)
-    assert(sz(edges) == 0);
-  else
-    assert(sz(edges) == max(num_zero_in, num_zero_out));
-  vector<vector<int>> adj_copy(adj);
-  for (auto [u, v] : edges) {
-    assert(u != v);
-    adj_copy[u].push_back(v);
+  {
+    // tests add_edges_strongly_connected
+    vector<bool> is_zero_in(num_sccs, 1), is_zero_out(num_sccs, 1);
+    for (int i = 0; i < n; i++) {
+      for (int v : adj[i]) {
+        if (scc_id[i] == scc_id[v]) continue;
+        is_zero_in[scc_id[v]] = 0;
+        is_zero_out[scc_id[i]] = 0;
+      }
+    }
+    // since {num_sccs-1, ..., 2, 1, 0} is a topo order
+    assert(is_zero_in[num_sccs - 1] && is_zero_out[0]);
+    int num_zero_in = int(count(begin(is_zero_in), end(is_zero_in), 1));
+    int num_zero_out = int(count(begin(is_zero_out), end(is_zero_out), 1));
+    vector<pair<int, int>> edges = extra_edges(adj, num_sccs, scc_id);
+    if (num_sccs == 1)
+      assert(sz(edges) == 0);
+    else
+      assert(sz(edges) == max(num_zero_in, num_zero_out));
+    vector<vector<int>> adj_copy(adj);
+    for (auto [u, v] : edges) {
+      assert(u != v);
+      adj_copy[u].push_back(v);
+    }
+    sccs curr_res(adj_copy);
+    assert(curr_res.num_sccs == 1);
   }
-  sccs curr_res(adj_copy);
-  assert(curr_res.num_sccs == 1);
 }
