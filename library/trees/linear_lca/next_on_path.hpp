826786e604b728476a79c62c3efada6c4633e131
Luke Videckis <lrvideckis@gmail.com>
2024-07-19 09:57:18 -0400

    nit (#25)
    * nit
    
    * [auto-verifier] verify commit 344787e8d840acc6ba1ba67fea4c4f1d53115d95
    
    ---------
    
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>
    Co-authored-by: GitHub <noreply@github.com>

--- library/trees/linear_lca/next_on_path.hpp
+++ library/trees/linear_lca/next_on_path.hpp
@@ -15,7 +15,7 @@ inline int next_on_path(int u, int v) {
   if ((t[u].asc & t[v].asc) != t[u].asc) return t[u].p;
   if (t[u].asc != t[v].asc) {
     auto k = bit_floor(t[u].asc ^ t[v].asc);
-    v = head[(t[v].label & -k) | k];
+    v = t[((t[v].label & -k) | k) - 1].head;
   }
   if (t[u].label != t[t[v].p].label || t[u].d >= t[v].d) return t[u].p;
   return t[v].p == u ? v : t[u].big_ch;
