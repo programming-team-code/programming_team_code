82993174899c9bf85d1289c568e76b3be37c0644
Luke Videckis <lrvideckis@gmail.com>
2024-07-23 15:39:17 -0500

    split apart grow (#31)
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>

--- library/math/n_choose_k/n_choose_k.hpp
+++ library/math/n_choose_k/n_choose_k.hpp
@@ -1,10 +1,6 @@
 /** @file */
 #pragma once
-const int mod = 17; /**< must be prime */
-struct comb {
-  ll inv = 1, fact = 1, inv_fact = 1;
-};
-vector<comb> t(2);
+#include "grow.hpp"
 /**
  * @param n,k integers with n < mod
  * @returns number of ways to choose k objects out of n
@@ -15,9 +11,6 @@ vector<comb> t(2);
 ll C(int n, int k) {
   assert(n < mod);
   if (k < 0 || n < k) return 0;
-  while (sz(t) <= n) {
-    ll i = sz(t), inv = mod - (mod / i) * t[mod % i].inv % mod;
-    t.push_back({inv, i * t[i - 1].fact % mod, inv * t[i - 1].inv_fact % mod});
-  }
+  grow(n);
   return t[n].fact * t[k].inv_fact % mod * t[n - k].inv_fact % mod;
 }
