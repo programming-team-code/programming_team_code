e371c785ac278b01a3993b867edd61893cc38f54
Luke Videckis <lukevideckis@gmail.com>
2024-07-24 13:41:29 -0500

    another lin rmq nit, and rerun all tests

--- library/data_structures/uncommon/linear_rmq.hpp
+++ library/data_structures/uncommon/linear_rmq.hpp
@@ -7,9 +7,9 @@
  */
 template <class T, class F> struct linear_rmq {
   vector<T> a;
+  int n;
   F cmp;
-  vi head;
-  vector<pii> t;
+  vi label, asc, head;
   /**
    * @code{.cpp}
          vector<ll> a(n);
@@ -22,23 +22,22 @@ template <class T, class F> struct linear_rmq {
    * @time O(n)
    * @space O(n)
    */
-  linear_rmq(const vector<T>& a_a, F a_cmp) : a(a_a), cmp(a_cmp), head(sz(a) + 1), t(sz(a)) {
+  linear_rmq(const vector<T>& a_a, F a_cmp) : a(a_a), n(sz(a)), cmp(a_cmp), label(n), asc(n), head(n + 1) {
     vi st{-1};
-    for (int i = 0; i <= sz(a); i++) {
+    for (int i = 0; i <= n; i++) {
       int prev = -1;
-      while (st.back() != -1 && (i == sz(a) || !cmp(a[st.back()], a[i]))) {
+      while (st.back() != -1 && (i == n || !cmp(a[st.back()], a[i]))) {
         if (prev != -1) head[prev] = st.back();
         int pw2 = 1 << __lg((end(st)[-2] + 1) ^ i);
-        t[st.back()].first = prev = i & -pw2;
+        label[st.back()] = prev = i & -pw2;
         st.pop_back();
-        t[st.back() + 1].second |= pw2;
+        asc[st.back() + 1] |= pw2;
       }
       if (prev != -1) head[prev] = i;
       st.push_back(i);
     }
-    rep(i, 1, sz(a))
-        t[i]
-            .second = (t[i].second | t[i - 1].second) & -(t[i].first & -t[i].first);
+    rep(i, 1, n)
+        asc[i] = (asc[i] | asc[i - 1]) & -(label[i] & -label[i]);
   }
   /**
    * @param le,ri defines range [min(le, ri), max(le, ri)]
@@ -47,9 +46,9 @@ template <class T, class F> struct linear_rmq {
    * @space O(1)
    */
   int query_idx(int le, int ri) {
-    auto j = t[le].second & t[ri].second & -(1 << __lg((t[le].first ^ t[ri].first) | 1));
-    if (auto k = t[le].second ^ j; k) k = 1 << __lg(k), le = head[(t[le].first & -k) | k];
-    if (auto k = t[ri].second ^ j; k) k = 1 << __lg(k), ri = head[(t[ri].first & -k) | k];
+    auto j = asc[le] & asc[ri] & -(1 << __lg((label[le] ^ label[ri]) | 1));
+    if (auto k = asc[le] ^ j; k) k = 1 << __lg(k), le = head[(label[le] & -k) | k];
+    if (auto k = asc[ri] ^ j; k) k = 1 << __lg(k), ri = head[(label[ri] & -k) | k];
     return cmp(a[le], a[ri]) ? le : ri;
   }
   /**
