a79967b00928b7f9474827aec4f775aea4fd24b2
Luke Videckis <lukevideckis@gmail.com>
2024-07-24 23:57:21 -0500

    fix

--- tests/library_checker_aizu_tests/handmade_tests/count_paths_forest.test.cpp
+++ tests/library_checker_aizu_tests/handmade_tests/count_paths_forest.test.cpp
@@ -8,10 +8,10 @@
 
 #include "../../../library/trees/centroid_decomp/count_paths_per_length.hpp"
 
-vector<vector<int64_t>> naive(const vector<vector<int>>& adj, dsu_restorable& dsu) {
+vector<vector<ll>> naive(const vector<vector<int>>& adj, dsu_restorable& dsu) {
   tree_lift tl(adj);
   int n = sz(adj);
-  vector<vector<int64_t>> cnts_naive(n + 1, vector<int64_t>(n, 0));
+  vector<vector<ll>> cnts_naive(n + 1, vector<ll>(n, 0));
   for (int u = 0; u < n; u++) {
     for (int v = u; v < n; v++) {
       if (dsu.same_set(u, v)) {
@@ -39,13 +39,13 @@ int main() {
         adj[v].push_back(u);
       }
     }
-    vector<vector<int64_t>> cnts_naive = naive(adj, dsu);
+    vector<vector<ll>> cnts_naive = naive(adj, dsu);
     for (int k = 1; k <= n; k++)
       assert(count_paths_per_node(adj, k) == cnts_naive[k]);
-    vector<int64_t> num_paths_len = count_paths_per_length(adj);
+    vector<ll> num_paths_len = count_paths_per_length(adj);
     for (int k = 1; k < n; k++) {
-      vector<int64_t> count_paths = count_paths_per_node(adj, k);
-      int64_t total_paths = accumulate(begin(count_paths), end(count_paths), 0LL);
+      vector<ll> count_paths = count_paths_per_node(adj, k);
+      ll total_paths = accumulate(begin(count_paths), end(count_paths), 0LL);
       assert(total_paths % (k + 1) == 0);
       total_paths /= k + 1;
       assert(num_paths_len[k] == total_paths);
