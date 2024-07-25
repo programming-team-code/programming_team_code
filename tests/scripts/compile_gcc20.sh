5dc7bd386e50c3443728451edb691101f39b1661
Luke Videckis <lukevideckis@gmail.com>
2024-07-24 23:55:08 -0500

    compile lib with actual kactl macros

--- tests/scripts/compile_gcc20.sh
+++ tests/scripts/compile_gcc20.sh
@@ -5,8 +5,9 @@ shopt -s globstar
 git submodule init
 git submodule update
 
-# remove ssize macro first
-sed --in-place '/ssize/d' library_checker_aizu_tests/template.hpp
+# compile lib with actual kactl macros
+# catches things like ll(x) (works with int64_t, but not with long long)
+cp library_checker_aizu_tests/actual_kactl_macros.hpp library_checker_aizu_tests/kactl_macros.hpp
 
 find library_checker_aizu_tests/ -type f -name "*.test.cpp" |
 	parallel g++ {} "$(tr '\n' ' ' <.config/.gcc_compile_flags)" -std=c++20 -Wno-narrowing
