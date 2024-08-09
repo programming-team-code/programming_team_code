4d5b2910f9c3b221406cdfdc916fee278ebbcd78
Luke Videckis <lukevideckis@gmail.com>
2024-08-09 09:52:07 -0500

    change interface

--- library/data_structures/bit_uncommon/lower_bound.hpp
+++ library/data_structures/bit_uncommon/lower_bound.hpp
@@ -1,13 +1,14 @@
 /** @file */
 #pragma once
 /**
- * Requires bit.sum(i, i + 1) >= 0 and sum > 0
+ * Requires bit.sum(i, i + 1) >= 0
  * @param sum see return
- * @returns max pos such that sum of range [0, pos) < sum
+ * @returns max pos such that sum of range [0, pos) < sum (or -1 when sum <= 0)
  * @time O(log n)
  * @space O(1)
  */
 int lower_bound(T sum) {
+  if (sum <= 0) return -1;
   int pos = 0;
   for (int pw = 1 << __lg(sz(s) | 1); pw; pw >>= 1)
     if (pos + pw <= sz(s) && s[pos + pw - 1] < sum)
