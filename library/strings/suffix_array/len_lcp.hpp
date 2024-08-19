8ae52a039f05301aebb59f9bd7dc99e7a1b4c9cb
Luke Videckis <lukevideckis@gmail.com>
2024-08-19 12:04:56 -0500

    run clang format

--- library/strings/suffix_array/len_lcp.hpp
+++ library/strings/suffix_array/len_lcp.hpp
@@ -7,8 +7,7 @@ template <class T> struct sa_query {
   int n;
   vi sa, sa_inv, lcp;
   RMQ<int, function<int(int, int)>> rmq;
-  sa_query(const T& a_s, const array<vi, 3>& x) : s(a_s), n(sz(s)), sa(x[0]), sa_inv(x[1]), lcp(x[2]),
-    rmq(lcp, [](int x, int y) { return min(x, y); }) {}
+  sa_query(const T& a_s, const array<vi, 3>& x) : s(a_s), n(sz(s)), sa(x[0]), sa_inv(x[1]), lcp(x[2]), rmq(lcp, [](int x, int y) { return min(x, y); }) {}
   sa_query(const T& a_s, int max_val) : sa_query(a_s, get_sa(a_s, max_val)) {}
   /**
    * @param i1,i2 defines suffixes [i1, n), [i2, n)
