675726e2ca79ad377986f4d893e59858d505db53
Luke Videckis <lukevideckis@gmail.com>
2024-08-20 03:18:56 -0500

    reduce scope

--- library/strings/suffix_array/find/find_string_bs_fast.hpp
+++ library/strings/suffix_array/find/find_string_bs_fast.hpp
@@ -11,10 +11,13 @@
 pii find_str_fast(const T& t) {
   int prev_idx = -1;
   int cnt_matched_prev = 0;
-  bool cond = 0;
   int le = lower_bound(all(sa), 0, [&](int i, int) -> bool {
              int curr_len_lcp = prev_idx == -1 ? 0 : len_lcp(i, prev_idx);
              if (cnt_matched_prev == sz(t)) return curr_len_lcp < sz(t);
+             bool cond = 0;
+             if (prev_idx != -1) {
+               cond = prev_idx + cnt_matched_prev == n || s[prev_idx + cnt_matched_prev] < t[cnt_matched_prev];
+             }
              if (cnt_matched_prev < curr_len_lcp) {
                return cond;
              } else if (cnt_matched_prev > curr_len_lcp) {
@@ -22,7 +25,7 @@ pii find_str_fast(const T& t) {
              } else {
                int cnt_matched_mid = mismatch(cnt_matched_prev + all(t), i + cnt_matched_prev + all(s)).first - begin(t);
                prev_idx = i, cnt_matched_prev = cnt_matched_mid;
-               return cond = cnt_matched_mid < sz(t) && (i + cnt_matched_mid == n || s[i + cnt_matched_mid] < t[cnt_matched_mid]);
+               return cnt_matched_mid < sz(t) && (i + cnt_matched_mid == n || s[i + cnt_matched_mid] < t[cnt_matched_mid]);
              }
            }) -
            begin(sa);
