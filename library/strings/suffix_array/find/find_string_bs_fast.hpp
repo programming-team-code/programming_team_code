3c9bd899b13803a8ba69448987b8fc93bf0bfc82
Luke Videckis <lukevideckis@gmail.com>
2024-08-19 23:32:35 -0500

    finish one todo

--- library/strings/suffix_array/find/find_string_bs_fast.hpp
+++ library/strings/suffix_array/find/find_string_bs_fast.hpp
@@ -9,45 +9,66 @@
  * @space O(1)
  */
 pii find_str_fast(const T& t) {
-  // TODO: convert to lower_bound?
   // TODO: combine both cases into one
   // TODO: make it not depend on find_substr
   if (empty(t)) return {0, n};
-  int le = 0, ri = n - 1;
+  int found = -1;
+  int le = sa[0], ri = sa.back();
   int cnt_matched_le = mismatch(all(t), sa[0] + all(s)).first - begin(t);
   int cnt_matched_ri = mismatch(all(t), sa.back() + all(s)).first - begin(t);
-  while (le + 1 < ri) {
-    int mid = (le + ri) / 2;
+  auto _ = lower_bound(all(sa), 0, [&](int i, int) -> bool {
     if (cnt_matched_le >= cnt_matched_ri) {
-      int curr_len_lcp = len_lcp(sa[le], sa[mid]);
-      if (cnt_matched_le < curr_len_lcp) le = mid;
-      else if (cnt_matched_le > curr_len_lcp) ri = mid;
-      else {
-        assert(equal(begin(t), begin(t) + cnt_matched_le, sa[mid] + begin(s)));
-        int cnt_matched_mid = mismatch(cnt_matched_le + all(t), sa[mid] + cnt_matched_le + all(s)).first - begin(t);
-        if (cnt_matched_mid == sz(t)) return find_substr(sa[mid], sa[mid] + sz(t));
-        if (sa[mid] + cnt_matched_mid == n || s[sa[mid] + cnt_matched_mid] < t[cnt_matched_mid])
-          le = mid, cnt_matched_le = cnt_matched_mid;
-        else
-          ri = mid, cnt_matched_ri = cnt_matched_mid;
+      int curr_len_lcp = len_lcp(i, le);
+      if (cnt_matched_le < curr_len_lcp) {
+        le = i;
+        return 1;
+      } else if (cnt_matched_le > curr_len_lcp) {
+        ri = i;
+        return 0;
+      } else {
+        assert(equal(begin(t), begin(t) + cnt_matched_le, i + begin(s)));
+        int cnt_matched_mid = mismatch(cnt_matched_le + all(t), i + cnt_matched_le + all(s)).first - begin(t);
+        if (cnt_matched_mid == sz(t)) {
+          found = i;
+          return 0;
+        }
+        if (i + cnt_matched_mid == n || s[i + cnt_matched_mid] < t[cnt_matched_mid]) {
+          le = i, cnt_matched_le = cnt_matched_mid;
+          return 1;
+        } else {
+          ri = i, cnt_matched_ri = cnt_matched_mid;
+          return 0;
+        }
       }
     } else {
-      int curr_len_lcp = len_lcp(sa[ri], sa[mid]);
-      if (cnt_matched_ri < curr_len_lcp) ri = mid;
-      else if (cnt_matched_ri > curr_len_lcp) le = mid;
-      else {
-        assert(equal(begin(t), begin(t) + cnt_matched_ri, sa[mid] + begin(s)));
-        int cnt_matched_mid = mismatch(cnt_matched_ri + all(t), sa[mid] + cnt_matched_ri + all(s)).first - begin(t);
-        if (cnt_matched_mid == sz(t)) return find_substr(sa[mid], sa[mid] + sz(t));
-        if (sa[mid] + cnt_matched_mid < n && s[sa[mid] + cnt_matched_mid] > t[cnt_matched_mid])
-          ri = mid, cnt_matched_ri = cnt_matched_mid;
-        else
-          le = mid, cnt_matched_le = cnt_matched_mid;
+      int curr_len_lcp = len_lcp(ri, i);
+      if (cnt_matched_ri < curr_len_lcp) {
+        ri = i;
+        return 0;
+      } else if (cnt_matched_ri > curr_len_lcp) {
+        le = i;
+        return 1;
+      } else {
+        assert(equal(begin(t), begin(t) + cnt_matched_ri, i + begin(s)));
+        int cnt_matched_mid = mismatch(cnt_matched_ri + all(t), i + cnt_matched_ri + all(s)).first - begin(t);
+        if (cnt_matched_mid == sz(t)) {
+          found = i;
+          return 1;
+        }
+        if (i + cnt_matched_mid < n && s[i + cnt_matched_mid] > t[cnt_matched_mid]) {
+          ri = i, cnt_matched_ri = cnt_matched_mid;
+          return 0;
+        } else {
+          le = i, cnt_matched_le = cnt_matched_mid;
+          return 1;
+        }
       }
     }
-  }
-  if (cnt_matched_le == sz(t)) return find_substr(sa[le], sa[le] + sz(t));
-  if (cnt_matched_ri == sz(t)) return find_substr(sa[ri], sa[ri] + sz(t));
+  });
+
+  if (found != -1) return find_substr(found, found + sz(t));
+  if (cnt_matched_le == sz(t)) return find_substr(le, le + sz(t));
+  if (cnt_matched_ri == sz(t)) return find_substr(ri, ri + sz(t));
   return {0, 0};
 }
 
