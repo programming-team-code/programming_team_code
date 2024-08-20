258f97808fac53045182f527fee9c2f71de0400d
Luke Videckis <lukevideckis@gmail.com>
2024-08-20 11:06:42 -0500

    rename vars

--- library/strings/suffix_array/find/find_string_bs_fast.hpp
+++ library/strings/suffix_array/find/find_string_bs_fast.hpp
@@ -9,21 +9,21 @@
  * @space O(1)
  */
 pii find_str_fast(const T& t) {
-  int prev_idx = n;
-  int cnt_matched_prev = 0;
-  bool prev_cond = 0;
+  int idx = n;
+  int cnt = 0;
+  bool s_less = 0;
   int le = lower_bound(all(sa), 0, [&](int i, int) -> bool {
-             int curr_len_lcp = prev_idx == n ? 0 : len_lcp(i, prev_idx);
-             if (cnt_matched_prev == sz(t)) return curr_len_lcp < sz(t);
-             if (cnt_matched_prev == curr_len_lcp) {
-               auto [it_s, it_t] = mismatch(i + cnt_matched_prev + all(s), cnt_matched_prev + all(t));
-               prev_idx = i, cnt_matched_prev = it_t - begin(t);
-               return prev_cond = it_t != end(t) && (it_s == end(s) || *it_s < *it_t);
+             int curr_len_lcp = idx == n ? 0 : len_lcp(i, idx);
+             if (cnt == sz(t)) return curr_len_lcp < sz(t);
+             if (cnt == curr_len_lcp) {
+               auto [it_s, it_t] = mismatch(i + cnt + all(s), cnt + all(t));
+               idx = i, cnt = it_t - begin(t);
+               return s_less = it_t != end(t) && (it_s == end(s) || *it_s < *it_t);
              }
-             if (cnt_matched_prev < curr_len_lcp) {
-               return prev_cond;
+             if (cnt < curr_len_lcp) {
+               return s_less;
              } else {
-               return !prev_cond;
+               return !s_less;
              }
            }) -
            begin(sa);
@@ -31,7 +31,7 @@ pii find_str_fast(const T& t) {
   int le_naive = lower_bound(begin(sa), end(sa), 0, [&](int i, int) -> bool { return lexicographical_compare(i + begin(s), end(s), begin(t), end(t)); }) - begin(sa);
   assert(le_naive == le);
 
-  if (cnt_matched_prev < sz(t)) return {le, le};
+  if (cnt < sz(t)) return {le, le};
   int ri = lower_bound(le + all(sa), 0, [&](int i, int) -> bool { return len_lcp(i, sa[le]) >= sz(t); }) - begin(sa);
   return {le, ri};
 }
