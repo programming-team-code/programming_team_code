775c1b5df1910adef64e1bc0c6c6c4235ff23fd4
Luke Videckis <lukevideckis@gmail.com>
2024-08-20 15:54:02 -0500

    run not in debug mode, and comment reason

--- tests/library_checker_aizu_tests/handmade_tests/string_with_vector.test.cpp
+++ tests/library_checker_aizu_tests/handmade_tests/string_with_vector.test.cpp
@@ -1,4 +1,6 @@
 #define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
+// since find_str_fast uses lower_bound in a weird way which makes the partition check fail
+#undef _GLIBCXX_DEBUG
 #include "../template.hpp"
 
 #include "../../../library/strings/suffix_array/suffix_array_query.hpp"
