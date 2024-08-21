de576a28112534d2debc69d92bcc3dced4903668
Luke Videckis <lukevideckis@gmail.com>
2024-08-21 11:19:49 -0600

    let's just do it this way

--- library/strings/suffix_array/find/find_string_bs_fast.hpp
+++ library/strings/suffix_array/find/find_string_bs_fast.hpp
@@ -11,13 +11,14 @@
 match find_str_fast(const T& t) {
   int s_le = n, s_len = 0;
   bool s_less = 0;
-  int sa_le = lower_bound(all(sa), 0, [&](int i, int) -> bool {
+  auto cmp = [&](int i, int) -> bool {
     if (int lcp_len = s_le == n ? 0 : len_lcp(i, s_le); s_len != lcp_len)
       return s_less ^ (s_len > lcp_len);
     auto [it_s, it_t] = mismatch(i + s_len + all(s), s_len + all(t));
     s_le = i, s_len = it_t - begin(t);
     return s_less = lexicographical_compare(it_s, end(s), it_t, end(t));
-  }) - begin(sa);
+  };
+  int sa_le = lower_bound(all(sa), 0, cmp) - begin(sa);
   if (s_len < sz(t)) return {sa_le, sa_le, s_le, s_le + s_len};
   int sa_ri = lower_bound(sa_le + all(sa), 0, [&](int i, int) -> bool { return len_lcp(i, s_le) >= sz(t); }) - begin(sa);
   return {sa_le, sa_ri, s_le, s_le + s_len};
