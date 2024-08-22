cb8979b042478154346716016933119ad1cd108b
Luke Videckis <lukevideckis@gmail.com>
2024-08-22 09:49:23 -0600

    style preference

--- library/strings/suffix_array/find/find_string_bs_fast.hpp
+++ library/strings/suffix_array/find/find_string_bs_fast.hpp
@@ -12,7 +12,7 @@ match find_str_fast(const T& t) {
   int s_le = n, s_len = 0;
   auto cmp = [&](int i, int) -> bool {
     if (int lcp_len = s_le == n ? 0 : len_lcp(i, s_le); s_len != lcp_len)
-      return (s_len > lcp_len) ^ (sa_inv[i] > sa_inv[s_le]);
+      return (s_len < lcp_len) ^ (sa_inv[i] < sa_inv[s_le]);
     auto [it_s, it_t] = mismatch(i + s_len + all(s), s_len + all(t));
     s_le = i, s_len = it_t - begin(t);
     return lexicographical_compare(it_s, end(s), it_t, end(t));
