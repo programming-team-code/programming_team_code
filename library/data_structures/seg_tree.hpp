4cb4fe9c4b83caf28acccc2568ba7c2977c604a6
Luke Videckis <lukevideckis@gmail.com>
2024-08-24 11:27:06 -0600

    add documentation

--- library/data_structures/seg_tree.hpp
+++ library/data_structures/seg_tree.hpp
@@ -1,5 +1,15 @@
 /** @file */
 #pragma once
+/**
+ * @see https://github.com/kth-competitive-programming/kactl /blob/main/content/data-structures/SegmentTree.h
+ * @code{.cpp}
+       tree st(n, INT_MAX, [&](int x, int y) -> int { return min(x, y); });
+       vector<array<int, 2>> a(n);
+       tree st(a, {1, 0}, [&](const array<int, 2>& le, const array<int, 2>& ri) -> array<int, 2> {
+           return {int(1LL * le[0] * ri[0] % mod), int((1LL * ri[0] * le[1] + ri[1]) % mod)};
+       });
+ * @endcode
+ */
 template <class T, class F> struct tree {
   int n;
   T unit;
@@ -11,7 +21,7 @@ template <class T, class F> struct tree {
     for (s[pos += n] = val; pos /= 2;)
       s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
   }
-  T query(int b, int e) {
+  T query(int b, int e) {  // query [b, e)
     T ra = unit, rb = unit;
     for (b += n, e += n; b < e; b /= 2, e /= 2) {
       if (b % 2) ra = f(ra, s[b++]);
