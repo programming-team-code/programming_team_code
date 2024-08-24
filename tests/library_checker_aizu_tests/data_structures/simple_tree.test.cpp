02e9e7eb02c96b0c2fab605b13434e18fdebe320
Luke Videckis <lukevideckis@gmail.com>
2024-08-24 11:12:16 -0600

    add other test

---
+++ tests/library_checker_aizu_tests/data_structures/simple_tree.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_A"
+#include "../template.hpp"
+
+#include "../../../library/data_structures/seg_tree.hpp"
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int n, q;
+  cin >> n >> q;
+  tree st(n, INT_MAX, [&](int x, int y) -> int { return min(x, y); });
+  while (q--) {
+    int type;
+    cin >> type;
+    if (type == 0) {
+      int pos, new_val;
+      cin >> pos >> new_val;
+      st.update(pos, new_val);
+    } else {
+      int le, ri;
+      cin >> le >> ri;
+      ri++;
+      cout << st.query(le, ri) << '\n';
+    }
+  }
+  return 0;
+}
+
