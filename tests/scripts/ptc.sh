7100767933d98afe1f06793530d80732b414afa6
Luke Videckis <lukevideckis@gmail.com>
2024-09-16 11:49:33 -0700

    format script

--- tests/scripts/ptc.sh
+++ tests/scripts/ptc.sh
@@ -35,7 +35,7 @@ for header in ../library/**/*.hpp; do
 		hash=$(head --lines "$i" "$header" | sed '/^#include/d' | cpp -dD -P -fpreprocessed | ./../library/contest/hash.sh)
 		line_length=$(sed --quiet "${i}p" "$header" | wc --chars)
 		padding_length=$((60 - 8 - line_length))
-		padding_length=$(( padding_length > 0 ? padding_length : 0 ))
+		padding_length=$((padding_length > 0 ? padding_length : 0))
 		padding=$(printf '%*s' "$padding_length" '')
 		sed --in-place "${i}s/$/$padding\/\/${hash}/" "$header"
 	done
