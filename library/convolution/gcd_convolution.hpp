10281f68706974c4a7c1fe6bd8befb4f39b6aaea
Luke Videckis <lukevideckis@gmail.com>
2024-09-17 20:16:16 -0700

    update these comments

--- library/convolution/gcd_convolution.hpp
+++ library/convolution/gcd_convolution.hpp
@@ -1,10 +1,10 @@
 #pragma once
-const int mod = 998'244'353;
 //! @param a,b arrays of the same length
-//! @returns array `c` where `c[k]` = the sum of (a[i] * b[j]) for all pairs
-//! (i,j) where gcd(i,j) == k
+//! @returns array `c` where `c[k]` = the sum of
+//! (a[i]*b[j]) for all pairs (i,j) where gcd(i,j)==k
 //! @time O(n log n)
-//! @space a size O(n) vector is allocated and returned
+//! @space O(n)
+const int mod = 998'244'353;
 vi gcd_convolution(const vi& a, const vi& b) {
   int n = sz(a);
   vi c(n);
