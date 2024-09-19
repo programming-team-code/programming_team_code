9aa2f1a05d9e61636759db00dae48cb016f69b42
Luke Videckis <lukevideckis@gmail.com>
2024-09-19 00:32:53 -0700

    updated init method

--- library/contest/hash.sh
+++ library/contest/hash.sh
@@ -1,6 +1,7 @@
 # Don't type comments nor #include "..."
 # Do type asserts
 
-# cat a.cpp | bash hash.sh
-# cat a.cpp | tr -d '[:space:]' | md5sum | cut -c-6
+# hash of first 5 lines
+# head -n 5 a.cpp | bash hash.sh
+# head -n 5 a.cpp | tr -d '[:space:]' | md5sum | cut -c-6
 tr -d '[:space:]' | md5sum | cut -c-6
