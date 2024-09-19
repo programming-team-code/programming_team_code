58085c9eafdd464e60e1b671d23647d9eee4bd6d
Luke Videckis <lukevideckis@gmail.com>
2024-09-19 00:54:27 -0700

    nit

--- library/math/fibonacci.hpp
+++ library/math/fibonacci.hpp
@@ -3,12 +3,12 @@ const int mod = 998'244'353;
 //! https://codeforces.com/blog/entry/14516
 //! https://cp-algorithms.com/algebra/fibonacci-numbers.html#fast-doubling-method
 //! @code
-//!     ll num = fib(n)[0];
-//!     //n=0 -> num=0
-//!     //n=1 -> num=1
-//!     //n=2 -> num=1
-//!     //n=3 -> num=2
-//!     //n=4 -> num=3
+//!   ll num = fib(n)[0];
+//!   //n=0 -> num=0
+//!   //n=1 -> num=1
+//!   //n=2 -> num=1
+//!   //n=3 -> num=2
+//!   //n=4 -> num=3
 //! @endcode
 //! @time O(log n)
 //! @space O(log n)
