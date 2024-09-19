220968f22de04fde25dee82a0049aed276b27418
Luke Videckis <lukevideckis@gmail.com>
2024-09-19 13:22:01 -0700

    remove iterate subtree

--- library/trees/lca_rmq/iterate_subtree.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "lca_rmq.hpp"
-//! https://codeforces.com/blog/entry/44351
-//! @time O(size of v's subtree)
-//! @space O(1)
-rep(k, lc.t[v].in, lc.t[v].in + lc.t[v].sub_sz) {
-  int u = lc.rmq.dp[0][k];
