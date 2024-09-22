78a96736911182c3948c4e97665f3844de90d85c
Luke Videckis <lukevideckis@gmail.com>
2024-09-22 13:31:09 -0500

    run format

--- tests/scripts/compile_commented_snippets.sh
+++ tests/scripts/compile_commented_snippets.sh
@@ -10,13 +10,15 @@ touch entire_library.cpp
 # build cpp file with everything in the library with all code in @code ...
 # @endcode comments in main
 
-echo "#include <bits/stdc++.h>" >> entire_library.cpp
-echo "using namespace std;" >> entire_library.cpp
-cat library_checker_aizu_tests/actual_kactl_macros.hpp >> entire_library.cpp
-echo "const ll mod = (119 << 23) + 1, root = 62;" >> entire_library.cpp
-find ../library/ -type f -name "*.hpp" | grep --invert-match --file=.config/.code_snippet_excluded_file_list | sort | sed 's/^/#include "/; s/$/"/' | cpp -std=c17 -nostdinc -C -P | grep --invert-match --extended-regexp "const int mod = |const ll mod = " >> entire_library.cpp
-echo "int main() {" >> entire_library.cpp
-echo "return 0;" >> entire_library.cpp
-echo "}" >> entire_library.cpp
+{
+	echo "#include <bits/stdc++.h>"
+	echo "using namespace std;"
+	cat library_checker_aizu_tests/actual_kactl_macros.hpp
+	echo "const ll mod = (119 << 23) + 1, root = 62;"
+	find ../library/ -type f -name "*.hpp" | grep --invert-match --file=.config/.code_snippet_excluded_file_list | sort | sed 's/^/#include "/; s/$/"/' | cpp -std=c17 -nostdinc -C -P | grep --invert-match --extended-regexp "const int mod = |const ll mod = "
+	echo "int main() {"
+	echo "return 0;"
+	echo "}"
+} >>entire_library.cpp
 
 sed --quiet '/\/\/! @code$/,/\/\/! @endcode$/{//!p;}' ../library/data_structures/bit.hpp
