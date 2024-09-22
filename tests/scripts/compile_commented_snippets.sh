e99fa13bbce32423ad15498d7321db26af68efd5
Luke Videckis <lukevideckis@gmail.com>
2024-09-21 22:45:21 -0500

    saving progress

--- tests/scripts/compile_commented_snippets.sh
+++ tests/scripts/compile_commented_snippets.sh
@@ -2,4 +2,21 @@
 # ** glob now searches any number of levels
 shopt -s globstar
 
-echo "hello world"
+
+# the flags we want:
+cpp -std=c17 -nostdinc -P ../library/data_structures/bit.hpp
+
+
+
+
+# 
+
+
+
+for header in ../library/**/*.hpp; do
+	# see https://github.com/programming-team-code/kactl_no_macros/blob/main/remove_macros.sh
+	cpp -std=c17 -nostdinc -C -P "$header" | cat library_checker_aizu_tests/kactl_macros.hpp - | cpp -std=c17 -nostdinc -C -P - -o "${header/\/library/}"
+done
+
+
+hash=$(head --lines "$i" "$header" | sed '/^#include/d' | cpp -dD -P -fpreprocessed | ./../library/contest/hash.sh)
