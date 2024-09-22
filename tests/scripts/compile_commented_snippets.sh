d5afcafc0a042f3b89ecbc357d3c7e89d1d1d788
Luke Videckis <lukevideckis@gmail.com>
2024-09-22 15:31:46 -0500

    add vars now

--- tests/scripts/compile_commented_snippets.sh
+++ tests/scripts/compile_commented_snippets.sh
@@ -15,6 +15,15 @@ git submodule update
 	echo "const ll mod = (119 << 23) + 1, root = 62;"
 	find ../library/ -type f -name "*.hpp" | grep --invert-match --file=.config/.code_snippet_excluded_file_list | sort | sed 's/^/#include "/; s/$/"/' | cpp -std=c17 -nostdinc -C -P | grep --invert-match --extended-regexp "const int mod = |const ll mod = "
 	echo "int main() {"
+	echo "vi a,b;"
+	echo "vl left,bottom;"
+	echo "vector<vi> adj;"
+	echo "vector<vector<pair<int,ll>>> adj_w;"
+  echo "vector<pii> edges;"
+  echo "vector<array<int,3>> w_eds;"
+  echo "vector<vector<mint>> mat;"
+  echo "vector<vector<bool>> grid;"
+	echo "int n,m,k,le,ri,root_l,root_r,source,sink,total_flow,bccid,v,rsz,cols;"
 } >entire_library_without_main
 
 {
