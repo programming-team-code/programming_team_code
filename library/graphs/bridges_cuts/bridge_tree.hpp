fb6c5d567be80e308705e1f0aa1fabd335961aef
Luke Videckis <lukevideckis@gmail.com>
2024-09-22 15:21:53 -0500

    another fix

--- library/graphs/bridges_cuts/bridge_tree.hpp
+++ library/graphs/bridges_cuts/bridge_tree.hpp
@@ -10,7 +10,7 @@ vector<vi> bridge_tree(const vector<vector<pii>>& adj,
   const bridges& br) {
   vector<vi> tree(br.num_ccs);
   rep(i, 0, sz(adj)) for (auto [u, e_id] : adj[i]) if (
-    br.is_bridge[e_id]) tree[br.cc_id[i]]
-    .push_back(br.cc_id[u]);
+    br.is_bridge[e_id]) tree[br.br_id[i]]
+    .push_back(br.br_id[u]);
   return tree;
 }
