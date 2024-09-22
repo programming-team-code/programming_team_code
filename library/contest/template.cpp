00bb00cba23698ea06e651da365ca9c97adf2b16
Luke Videckis <lukevideckis@gmail.com>
2024-09-21 22:18:03 -0500

    format now

--- library/contest/template.cpp
+++ library/contest/template.cpp
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define rep(i, a, b) for (int i = a; i < (b); i++)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)size(x)
 typedef int64_t ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
-
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   return 0;
