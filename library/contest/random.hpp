9afca20d502059628122bc66ac1effb57292fbf1
Luke Videckis <lukevideckis@gmail.com>
2024-09-22 14:59:11 -0500

    consistency

--- library/contest/random.hpp
+++ library/contest/random.hpp
@@ -2,10 +2,10 @@
 //! https://codeforces.com/blog/entry/61675
 //! @code
 //!   //mt19937_64 rng(chrono...);
-//!   uint64_t x = rng();
+//!   uint64_t x1 = rng();
 //!   mt19937 rng; // fixed seed for debugging
-//!   int y = rnd(0, 1); //random number in [0,1]
-//!   ll z = rnd<ll>(1, 1e18);
+//!   int x2 = rnd(0, 1); //random number in [0,1]
+//!   ll x3 = rnd<ll>(1, 1e18);
 //! @endcode
 mt19937 rng(
   chrono::steady_clock::now().time_since_epoch().count());
