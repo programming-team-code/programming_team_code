4a3c05dbd7d5d780728770768e5a091deda23098
Luke Videckis <lrvideckis@gmail.com>
2024-10-01 14:32:47 -0500

    Line tree (#71)
    * first draft of line tree
    
    * add test now
    
    * [auto-verifier] verify commit e2081d2b3ba953df7a936dce3886a9f5e04406fe
    
    * add lib checker test and nits
    
    * [auto-verifier] verify commit 9064be549f954f590c4c5c62c170e998d5f63179
    
    * add docs now
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ library/graphs/line_tree.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "../../kactl/content/data-structures/UnionFind.h"
+//! https://codeforces.com/blog/entry/71568?#comment-559304
+//! @code
+//!   sort(all(w_eds));
+//!   auto [llist, uf] = line_tree(w_eds, n);
+//!   for (int v = uf.find(0); v != -1;
+//!     v = llist[v].first) {}
+//! @endcode
+//! llist[v] = {next node, edge weight}
+//! uf.find(v) = head of linked list
+//!   of component containing v
+//! @time O(n + m * \alpha(n))
+//! @space O(n + m)
+pair<vector<pii>, UF> line_tree(
+    const vector<array<int, 3>>& w_eds, int n) {
+  vector<pii> list(n, {-1, -1});
+  vi last(n);
+  iota(all(last), 0);
+  UF uf(n);
+  for (auto [w, u, v] : w_eds) {
+    u = uf.find(u), v = uf.find(v);
+    if (uf.join(u, v)) {
+      int p = uf.find(u);
+      list[last[p]] = {p ^ u ^ v, w};
+      last[p] = last[p ^ u ^ v];
+    }
+  }
+  return {list, uf};
+}
---
+++ tests/library_checker_aizu_tests/graphs/line_tree_aizu.test.cpp
@@ -0,0 +1,44 @@
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_12_A"
+#include "../template.hpp"
+#include "../../../library/graphs/line_tree.hpp"
+#include "../../../library/data_structures/rmq.hpp"
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int n;
+  cin >> n;
+  vector<array<int, 3>> w_eds;
+  vector<vector<int>> mat(n, vector<int>(n));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> mat[i][j];
+      if (mat[i][j] == -1) {
+        mat[i][j] = INT_MAX;
+        continue;
+      }
+      w_eds.push_back({mat[i][j], i, j});
+    }
+  }
+  sort(all(w_eds));
+  auto [llist, uf] = line_tree(w_eds, n);
+  rep(k, 0, n) rep(i, 0, n) rep(j, 0, n) {
+    mat[i][j] = min(mat[i][j], max(mat[i][k], mat[k][j]));
+  }
+  int mst_sum = 0;
+  vector<int> edge_weights;
+  vector<int> to_time(n);
+  for (int v = uf.find(0), timer = 1; llist[v].first != -1;
+       v = llist[v].first, timer++) {
+    edge_weights.push_back(llist[v].second);
+    to_time[llist[v].first] = timer;
+    mst_sum += llist[v].second;
+  }
+  RMQ rmq(edge_weights,
+    [&](int x, int y) { return max(x, y); });
+  rep(i, 0, n) rep(j, i + 1, n) {
+    auto [i1, i2] = minmax(to_time[i], to_time[j]);
+    assert(rmq.query(i1, i2) == mat[i][j]);
+  }
+  cout << mst_sum << '\n';
+  return 0;
+}
