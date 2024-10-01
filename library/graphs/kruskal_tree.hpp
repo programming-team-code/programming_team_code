2f7d37c031298a52e735768de8c4d2de1e4f5b2f
Luke Videckis <lukevideckis@gmail.com>
2024-10-01 18:43:59 -0500

    adding kruskal tree

---
+++ library/graphs/kruskal_tree.hpp
@@ -0,0 +1,12 @@
+#pragma once
+struct kr_tree {
+  int id;
+  vi p;
+  vector<vi> adj;
+  kr_tree(int n) : id(n), p(2 * n, -1), adj(2 * n) {}
+  int find(int v) { return p[v] < 0 ? v : p[v] = find(p[v]); }
+  bool join(int u, int v) {
+    if ((u = find(u)) == (v = find(v))) return 0;
+    return adj[p[u] = p[v] = id++] = {u, v}, 1;
+  }
+};
---
+++ tests/library_checker_aizu_tests/graphs/kruskal_tree_aizu.test.cpp
@@ -0,0 +1,59 @@
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_12_A"
+#include "../template.hpp"
+#include "../../../library/graphs/kruskal_tree.hpp"
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int n;
+  cin >> n;
+  vector<array<int, 3>> w_eds;
+  vector<vector<int>> mat(n, vector<int>(n));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> mat[i][j];
+      if (mat[i][j] == -1) {
+        mat[i][j] = INT_MAX;
+        continue;
+      }
+      w_eds.push_back({mat[i][j], i, j});
+    }
+  }
+  sort(all(w_eds));
+  kr_tree kt(n);
+  vector<int> weight(2 * n);
+  for (auto [w, u, v] : w_eds) {
+    if (kt.join(u, v)) {
+      weight[kt.id - 1] = w;
+    }
+  }
+  rep(k, 0, n) rep(i, 0, n) rep(j, 0, n) {
+    mat[i][j] = min(mat[i][j], max(mat[i][k], mat[k][j]));
+  }
+  int mst_sum = 0;
+  for (int i = n; i < kt.id; i++) {
+    mst_sum += weight[i];
+  }
+  vector<int> depth(2 * n);
+  vector<int> actual_par(2 * n, -1);
+  auto dfs = [&](auto&& self, int v) -> void {
+    for (int u : kt.adj[v]) {
+      depth[u] = 1 + depth[v];
+      actual_par[u] = v;
+      self(self, u);
+    }
+  };
+  dfs(dfs, kt.id - 1);
+  rep(i, 0, n) rep(j, i + 1, n) {
+    int u = i, v = j;
+    while (u != v) {
+      if (depth[u] > depth[v]) {
+        u = actual_par[u];
+      } else {
+        v = actual_par[v];
+      }
+    }
+    assert(weight[u] == mat[i][j]);
+  }
+  cout << mst_sum << '\n';
+  return 0;
+}
