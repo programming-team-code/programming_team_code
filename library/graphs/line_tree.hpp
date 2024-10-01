46fa69a4788cedefdcdfc9f26667af11c4839fa2
Luke Videckis <lukevideckis@gmail.com>
2024-10-01 13:03:26 -0500

    first draft of line tree

---
+++ library/graphs/line_tree.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "../../kactl/content/data-structures/UnionFind.h"
+pair<vector<pii>, UF> line_tree(const vector<array<int, 3>>& w_eds) {
+  vector<pii> list(n, {-1, -1});  // list[v] = {next node, edge weight}
+  vi last(n);
+  iota(all(last), 0);
+  UF uf(n);
+  for (auto [w, u, v] : w_eds) {
+    u = uf.find(u), v = uf.find(v);
+    if (uf.join(u, v)) {
+      int p = uf.find(u);
+      list[last[p]] = {p ^ u ^ v, w};
+      last[p] = last[p ^ u ^ v];
+    }
+  }
+  return {list, uf};
+}
+
