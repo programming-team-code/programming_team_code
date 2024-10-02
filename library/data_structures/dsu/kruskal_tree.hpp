f07dc5c6f142d5defcfe48608070f313cd7a0993
Luke Videckis <lukevideckis@gmail.com>
2024-10-02 08:53:31 -0500

    revert name

--- library/data_structures/dsu/kruskal_tree.hpp
+++ library/data_structures/dsu/kruskal_tree.hpp
@@ -2,11 +2,11 @@
 //! https://mzhang2021.github.io/cp-blog/kruskal/
 //! @time O(n log n)
 //! @space O(n)
-struct kr_tree_lin_h {
+struct kr_tree {
   int id;
   vi p;
   vector<vi> adj;
-  kr_tree_lin_h(int n) : id(n), p(2 * n, -1), adj(2 * n) {}
+  kr_tree(int n) : id(n), p(2 * n, -1), adj(2 * n) {}
   int find(int v) {
     return p[v] < 0 ? v : p[v] = find(p[v]);
   }
