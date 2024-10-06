4a88c4ce4badc5febdab1c7ba02fb8ede521e815
Luke Videckis <lukevideckis@gmail.com>
2024-10-06 16:29:44 -0500

    no true's

--- library/trees/extra_members/in_subtree.hpp
+++ library/trees/extra_members/in_subtree.hpp
@@ -1,5 +1,5 @@
-//! returns true if v is in u's subtree
+//! returns 1 if v is in u's subtree
 bool in_subtree(int u, int v) {
   return t[u].in <= t[v].in &&
-    t[v].in < t[u].in + t[u].sub_sz;
+         t[v].in < t[u].in + t[u].sub_sz;
 }
--- library/trees/extra_members/on_path.hpp
+++ library/trees/extra_members/on_path.hpp
@@ -1,5 +1,5 @@
-//! returns true if w is on path from u to v
+//! returns 1 if w is on path from u to v
 bool on_path(int u, int v, int w) {
   return (in_subtree(w, u) || in_subtree(w, v)) &&
-    in_subtree(lca(u, v), w);
+         in_subtree(lca(u, v), w);
 }
