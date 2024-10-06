0d3c17c46130b82a4e2b7e5bbdae45da5ec3669e
Luke Videckis <lukevideckis@gmail.com>
2024-10-06 18:14:23 -0500

    add lazy seg constructor test

---
+++ tests/library_checker_aizu_tests/data_structures/lazy_segment_tree_inc_constructor.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM \
+  "https://judge.yosupo.jp/problem/point_add_range_sum"
+#include "../template.hpp"
+#include "../../../library/data_structures/lazy_seg_tree_inc.hpp"
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int n, q;
+  cin >> n >> q;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) cin >> a[i];
+  seg_tree_inc st(a);
+  while (q--) {
+    int type;
+    cin >> type;
+    if (type == 0) {
+      int idx;
+      int64_t x;
+      cin >> idx >> x;
+      st.update(idx, idx, x);
+    } else {
+      assert(type == 1);
+      int l, r;
+      cin >> l >> r;
+      cout << st.query(l, r - 1) << '\n';
+    }
+  }
+  return 0;
+}
