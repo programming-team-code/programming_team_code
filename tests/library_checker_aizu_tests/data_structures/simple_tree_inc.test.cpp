14479d023ad3b543e14cc3da181925f8f112b346
Luke Videckis <lukevideckis@gmail.com>
2024-10-06 18:37:35 -0500

    add simple seg inc

---
+++ tests/library_checker_aizu_tests/data_structures/simple_tree_inc.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_A"
+#include "../template.hpp"
+#include "../../../library/data_structures/seg_tree_inc.hpp"
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int n, q;
+  cin >> n >> q;
+  tree_inc st(n, INT_MAX,
+    [&](int x, int y) -> int { return min(x, y); });
+  while (q--) {
+    int type;
+    cin >> type;
+    if (type == 0) {
+      int pos, new_val;
+      cin >> pos >> new_val;
+      st.update(pos, new_val);
+    } else {
+      int le, ri;
+      cin >> le >> ri;
+      cout << st.query(le, ri) << '\n';
+    }
+  }
+  return 0;
+}
