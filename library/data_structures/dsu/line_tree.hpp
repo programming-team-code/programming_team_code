4ea2947b2782735d5233f574af60b362265e2cf0
Luke Videckis <lukevideckis@gmail.com>
2024-10-06 16:41:49 -0500

    finish first draft

--- library/data_structures/dsu/line_tree.hpp
+++ library/data_structures/dsu/line_tree.hpp
@@ -12,26 +12,11 @@
 //!   of component containing v
 //! @time O(n + m * \alpha(n))
 //! @space O(n + m)
-pair<vector<pii>, UF> line_tree(
-    const vector<array<int, 3>>& w_eds, int n) {
-  vector<pii> list(n, {-1, -1});
-  vi last(n);
-  iota(all(last), 0);
-  UF uf(n);
-  for (auto [w, u, v] : w_eds) {
-    u = uf.find(u), v = uf.find(v);
-    if (uf.join(u, v)) {
-      if (v != uf.find(v)) swap(u, v);
-      list[last[v]] = {u, w};
-      last[v] = last[u];
-    }
-  }
-  return {list, uf};
-}
-
 struct line_tree {
+  int id = 0;
   struct node {
-    int p = -1, next = -1, w_next, last;
+    int p = -1, last;
+    pii edge = {-1, -1};
   };
   vi t;
   line_tree(int n) : t(n) {
@@ -39,11 +24,11 @@ struct line_tree {
   }
   int size(int x) { return -t[find(x)].p; }
   int find(int x) { return t[x].p < 0 ? x : t[x].p = find(t[x].p); }
-  bool join(int a, int b) {
-    a = find(a), b = find(b);
-    if (a == b) return 0;
-    if (t[a].p > t[b].p) swap(a, b);
-    t[a].p += t[b].p, t[b].p = a;
-    return true;  // TODO add CI to check this is 1
+  bool join(int u, int v) {
+    u = find(u), v = find(v);
+    if (u == v) return 0;
+    if (t[u].p < t[v].p) swap(u, v);
+    t[v].p += t[u].p, t[u].p = v, t[exchange(t[v].last, t[u].last)].edge = {u, id++};
+    return true;
   }
 };
