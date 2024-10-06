78c23d7b257fa225ab9625ba9ec5a5f616a2528c
Luke Videckis <lukevideckis@gmail.com>
2024-10-06 18:01:28 -0500

    uneeded

--- library/data_structures/seg_tree_uncommon/tree_inc_vector_constructor.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-tree(const vector<T>& a, T a_unit, F a_f):
-  n(sz(a)), unit(a_unit), f(a_f), s(2 * n) {
-  rep(i, 0, n) s[i + n] = a[i];
-  for (int i = n - 1; i > 0; i--)
-    s[i] = f(s[2 * i], s[2 * i + 1]);
-}
