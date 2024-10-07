35afe4268455e77197e0cac994861abbbdb6fa69
Luke Videckis <lrvideckis@gmail.com>
2024-10-06 23:54:46 -0700

    fix

--- library/contest/tester.sh
+++ library/contest/tester.sh
@@ -1,5 +1,5 @@
 # https://github.com/Errichto/youtube/blob/master/testing/s.sh
-# bash tester_diff.sh a naive gen
+# bash tester.sh a naive gen
 
 g++ -std=c++20 -g -D_GLIBCXX_DEBUG -fsanitize=address,undefined $1.cpp -o $1.out
 g++ -std=c++20 $2.cpp -o $2.out
