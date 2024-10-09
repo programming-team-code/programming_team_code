05b44e965d6b251026c19ef1fd832e593e4f149f
Luke Videckis <lukevideckis@gmail.com>
2024-10-09 12:22:59 -0700

    fix

--- tests/library_checker_aizu_tests/flow/dinic_lib_checker.test.cpp
+++ tests/library_checker_aizu_tests/flow/dinic_lib_checker.test.cpp
@@ -8,20 +8,20 @@ int main() {
   cin >> l >> r >> m;
   const int source = l + r;
   const int sink = l + r + 1;
-  Dinic d(l + r + 2);
-  for (int i = 0; i < l; i++) d.addEdge(source, i, 1);
-  for (int i = 0; i < r; i++) d.addEdge(i + l, sink, 1);
+  dinic d(l + r + 2);
+  for (int i = 0; i < l; i++) d.add_edge(source, i, 1);
+  for (int i = 0; i < r; i++) d.add_edge(i + l, sink, 1);
   vector<int> u_node(m);
   vector<int> edge_ids(m);
   for (int i = 0; i < m; i++) {
     int u, v;
     cin >> u >> v;
     u_node[i] = u;
-    edge_ids[i] = d.addEdge(u, v + l, 1);
+    edge_ids[i] = d.add_edge(u, v + l, 1);
   }
   cout << d.calc(source, sink) << '\n';
   for (int i = 0; i < m; i++) {
-    Dinic::Edge an_edge = d.edge[edge_ids[i]];
+    dinic::edge an_edge = d.edges[edge_ids[i]];
     if (an_edge.flow() == 1)
       cout << u_node[i] << ' ' << an_edge.to - l << '\n';
   }
