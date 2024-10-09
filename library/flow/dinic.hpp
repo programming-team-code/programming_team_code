9c95f3a5848b305add98ff5ef17e4bce59dd74a5
Luke Videckis <lrvideckis@gmail.com>
2024-10-09 14:33:44 -0700

    Dinic (#80)
    * add dinic
    
    * [auto-verifier] verify commit 28ac1c6d42a3f199e5f91fa41790419a5a4891d9
    
    * add docs now
    
    * [auto-verifier] verify commit db8b21523b95704e65d4517719aa667d9c5b6954
    
    * check flow property
    
    * [auto-verifier] verify commit 95b7b3e38fc8491e0e3a975c9896826f8050e147
    
    * snake case
    
    * fix
    
    * [auto-verifier] verify commit ca7c87dfe533b4eff388d819046d160b7b93822c
    
    * [auto-verifier] verify commit 37c2555b3af32aa534cf965d6391d0dd07c3ad43
    
    * Update dinic.hpp
    
    * [auto-verifier] verify commit 6161c7d503433dbebc43d2e736eec27ab9c0fc61
    
    * Update dinic_aizu.test.cpp
    
    * [auto-verifier] verify commit 77ecc585a5388551cc3d0acec7beac7b43d7d8dc
    
    * fix name clash
    
    * fix code snippet
    
    * [auto-verifier] verify commit 63954d6f1a61d45eb62450ad1b16015122b04959
    
    * fix
    
    * now compiles
    
    * fix name clash
    
    * nit
    
    * [auto-verifier] verify commit 1401f785b0f669313040a1578ab00768b16eee32
    
    * check left of min cut now
    
    * [auto-verifier] verify commit b5cae0af00033ef73a3898f0eed7a19d31b161ab
    
    * I like this var name more
    
    * [auto-verifier] verify commit a48ec7a1c613211a532a9480958c682633a9c5d2
    
    * now adding asserts for both tests
    
    * fix
    
    * [auto-verifier] verify commit b671544fca57d53bcfe24fd91e60a8f37c0df9a7
    
    * make it harder to use back edges
    
    * now handles edge case
    
    * [auto-verifier] verify commit 5a5f49e2fb12a1a1472abe470da5c52c7d65da24
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ library/flow/dinic.hpp
@@ -0,0 +1,61 @@
+//! https://github.com/kth-competitive-programming/kactl/blob/main/content/graph/Dinic.h
+//! @code
+//!   const int src = n, snk = n + 1;
+//!   dinic dn(n + 2);
+//!   int e_id = dn.add_edge(u, v, cap);
+//!   ll tot_flow = dn.calc(src, snk);
+//!   dinic::edge an_edge = dn.edges[e_id];
+//! @endcode
+//! @time O(n * m * log(max_cap))
+//!       O(min(sqrt(m), n^(2/3)) * m) if max_cap==1
+//!       O(m * sqrt(m))
+//! @space O(n + m)
+struct dinic {
+  struct edge {
+    int v, to;
+    ll c, cap;
+    ll flow() { return cap - c; }
+  };
+  ll max_cap = 1;
+  vi lvl, ptr, q;
+  vector<vi> adj;
+  vector<edge> edges;
+  dinic(int n): lvl(n), ptr(n), q(n), adj(n) {}
+  int add_edge(int a, int b, ll c) {
+    adj[a].push_back(sz(edges));
+    edges.push_back({a, b, c, c});
+    adj[b].push_back(sz(edges));
+    edges.push_back({-1, a, 0, -1});
+    max_cap = max(max_cap, c);
+    return sz(edges) - 2;
+  }
+  ll dfs(int v, int t, ll f) {
+    if (v == t || !f) return f;
+    for (int& i = ptr[v]; i < sz(adj[v]); i++) {
+      edge& e = edges[adj[v][i]];
+      if (lvl[e.to] == lvl[v] + 1)
+        if (ll p = dfs(e.to, t, min(f, e.c))) {
+          e.c -= p, edges[adj[v][i] ^ 1].c += p;
+          return p;
+        }
+    }
+    return 0;
+  }
+  ll calc(int s, int t) {
+    ll flow = 0;
+    q[0] = s;
+    for (ll i = max_cap; i; i /= 2) do {
+        lvl = ptr = vi(sz(q));
+        int qi = 0, qe = lvl[s] = 1;
+        while (qi < qe && !lvl[t]) {
+          int v = q[qi++];
+          for (int id : adj[v])
+            if (edge e = edges[id]; !lvl[e.to] && e.c / i)
+              q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
+        }
+        while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
+      } while (lvl[t]);
+    return flow;
+  }
+  bool left_of_min_cut(int v) { return lvl[v]; }
+};
