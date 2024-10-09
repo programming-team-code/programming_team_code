a97b64e842e6850b65a4bc1956a04e4625ad46a9
Luke Videckis <lukevideckis@gmail.com>
2024-10-09 14:21:28 -0700

    fix

--- tests/library_checker_aizu_tests/flow/dinic_aizu.test.cpp
+++ tests/library_checker_aizu_tests/flow/dinic_aizu.test.cpp
@@ -8,13 +8,10 @@ int main() {
   int n, m;
   cin >> n >> m;
   dinic d(n);
-  vector<int> edge_ids(m);
-  vector<array<int, 3>> all_edges(m);
   for (int i = 0; i < m; i++) {
     int u, v, cap;
     cin >> u >> v >> cap;
-    all_edges[i] = {u, v, cap};
-    edge_ids[i] = d.add_edge(u, v, cap);
+    d.add_edge(u, v, cap);
   }
   ll total_flow = d.calc(0, n - 1);
   dinic_asserts(d, 0, n - 1, total_flow);
