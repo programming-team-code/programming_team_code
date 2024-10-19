9510c809aa8fe8c4ba7ab944545e8e6d0612cdc8
Luke Videckis <lukevideckis@gmail.com>
2024-10-19 00:00:26 -0700

    fix

--- library/strings/longest_common_subsequence/lcs_queries.hpp
+++ library/strings/longest_common_subsequence/lcs_queries.hpp
@@ -5,9 +5,9 @@
 //!   size(LCS(s[0,s_ri), t[t_le,t_ri)))
 //! @time O(n*m*log(m) + q*log(m) + q*log(q))
 //! @space O(n + m + q)
-template<class T>
+template <class T>
 vi lcs_queries(const T& s, const T& t,
-  const vector<array<int, 3>>& queries) {
+               const vector<array<int, 3>>& queries) {
   int n = sz(s), m = sz(t), q = sz(queries);
   vector<vector<array<int, 3>>> qs(n);
   rep(i, 0, q) {
@@ -19,12 +19,12 @@ vi lcs_queries(const T& s, const T& t,
   vi res(q);
   rep(i, 0, n) {
     lcs.push_onto_s(s[i]);
-    vi init(m), dp_inv(m, -1);
+    BIT bit(m);
+    vi dp_inv(m, -1);
     rep(j, 0, m) {
-      if (lcs.dp[j] == -1) init[j] = 1;
+      if (lcs.dp[j] == -1) bit.update(j, 1);
       else dp_inv[lcs.dp[j]] = j;
     }
-    BIT<int> bit(init);
     sort(all(qs[i]));
     int j = 0;
     rep(t_le, 0, m) {
