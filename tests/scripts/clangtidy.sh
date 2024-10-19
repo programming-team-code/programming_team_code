0e7098692db1ad848ddc9c5de18dd3e31975f96d
Luke Videckis <lukevideckis@gmail.com>
2024-10-19 01:11:51 -0700

    fix

--- tests/scripts/clangtidy.sh
+++ tests/scripts/clangtidy.sh
@@ -5,6 +5,8 @@ shopt -s globstar
 git submodule init
 git submodule update
 
+cp library_checker_aizu_tests/kactl_macros_gcc17.hpp library_checker_aizu_tests/kactl_macros_gcc20.hpp
+
 find library_checker_aizu_tests/ -type f -name "*.test.cpp" |
-	parallel clang-tidy --config-file=.config/.clang-tidy {} -- -std=c++20 ||
+	parallel clang-tidy --config-file=.config/.clang-tidy {} -- -std=c++17 ||
 	exit 1
