98fc133a1788aa55ccf2b0ee084dd404a35b9a88
Luke Videckis <lrvideckis@gmail.com>
2024-10-18 23:16:40 -0700

    Reapply "Ladder optimization (#83)" (#84)
    * Reapply "Ladder optimization (#83)"
    
    This reverts commit c0fa4a024c745f92a78920001e395c2d6065fa13.
    
    * [auto-verifier] verify commit d08957972a40d181e2a47f91201d71f852ae7051
    
    * saving progress
    
    * [auto-verifier] verify commit 30c5733e2e4f2103046fcb121280f6c82f1431b0
    
    * different style
    
    * final
    
    * add comment
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/trees/ladder_decomposition/ladder_decomposition.hpp
+++ library/trees/ladder_decomposition/ladder_decomposition.hpp
@@ -10,13 +10,13 @@
 //! @endcode
 struct ladder {
   int n;
-  vector<vi> b_tbl, l_tbl;
-  vi d, p, dl; //!< deepest leaf
+  vector<vi> b_tbl;
+  vi d, p, dl, idx_l, l_tbl;
   //! @param adj forest (rooted or unrooted)
   //! @time O(n log n)
   //! @space O(n log n) for b_tbl. Everything else is O(n)
   ladder(const vector<vi>& adj):
-    n(sz(adj)), l_tbl(n), d(n), p(n, -1), dl(n) {
+    n(sz(adj)), d(n), p(n, -1), dl(n), idx_l(n) {
     auto dfs = [&](auto&& self, int v) -> void {
       dl[v] = v;
       for (int u : adj[v])
@@ -29,11 +29,10 @@ struct ladder {
     rep(i, 0, n) {
       if (p[i] == -1) p[i] = i, dfs(dfs, i);
       if (p[i] == i || dl[p[i]] != dl[i]) {
-        int leaf = dl[i];
-        vi& lad = l_tbl[leaf];
-        lad.resize(min(2 * (d[leaf] - d[i]), d[leaf] + 1),
-          leaf);
-        rep(j, 1, sz(lad)) lad[j] = p[lad[j - 1]];
+        int v = dl[i], len = (d[v] - d[i]) * 2;
+        idx_l[v] = sz(l_tbl) + d[v];
+        for (; v != -1 && len--; v = p[v])
+          l_tbl.push_back(v);
       }
     }
     b_tbl = treeJump(p);
@@ -49,7 +48,10 @@ struct ladder {
     if (k == 0) return v;
     int bit = __lg(k);
     v = b_tbl[bit][v], k -= (1 << bit);
-    int leaf = dl[v];
-    return l_tbl[leaf][k + d[leaf] - d[v]];
+    int le = idx_l[dl[v]] - d[v];
+    assert(l_tbl[le] == v);
+    // subarray [le, le+k] of l corresponds to the rest of
+    // the jump
+    return l_tbl[le + k];
   }
 };
