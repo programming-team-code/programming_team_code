8e208b124905c9520b0f4dd9a0a75046037da086
Luke Videckis <lukevideckis@gmail.com>
2024-10-19 22:37:49 -0700

    fix

--- library/data_structures/uncommon/linear_rmq.hpp
+++ library/data_structures/uncommon/linear_rmq.hpp
@@ -1,28 +1,27 @@
 #pragma once
 //! https://codeforces.com/blog/entry/125371?#comment-1173604
 //! @code
-//!   linear_rmq r1(a, less());//right-most min
-//!   linear_rmq r2(a, less_equal());//left-most min
-//!   linear_rmq r3(a, greater());//right-most max
-//!   linear_rmq r4(a, greater_equal());//left-most max
-//!   linear_rmq r5(a, [&](auto& x, auto& y) {
+//!   linear_rmq lr1(a, less());//right-most min
+//!   linear_rmq lr2(a, less_equal());//left-most min
+//!   linear_rmq lr3(a, greater());//right-most max
+//!   linear_rmq lr4(a, greater_equal());//left-most max
+//!   linear_rmq lr5(a, [&](auto& x, auto& y) {
 //!     return x < y;
 //!   });
 //! @endcode
 //! @time O(n + q)
 //! @space O(n)
-template<class T, class F> struct linear_rmq {
+template <class T, class F> struct linear_rmq {
   vector<T> a;
   F cmp;
   vi head;
   vector<array<int, 2>> t;
-  linear_rmq(const vector<T>& a_a, F a_cmp):
-    a(a_a), cmp(a_cmp), head(sz(a) + 1), t(sz(a)) {
+  linear_rmq(const vector<T>& a_a, F a_cmp) : a(a_a), cmp(a_cmp), head(sz(a) + 1), t(sz(a)) {
     vi st{-1};
     for (int i = 0; i <= sz(a); i++) {
       int prev = -1;
       while (st.back() != -1 &&
-        (i == sz(a) || !cmp(a[st.back()], a[i]))) {
+             (i == sz(a) || !cmp(a[st.back()], a[i]))) {
         if (prev != -1) head[prev] = st.back();
         int pw2 = 1 << __lg((end(st)[-2] + 1) ^ i);
         t[st.back()][0] = prev = i & -pw2;
@@ -33,9 +32,9 @@ template<class T, class F> struct linear_rmq {
       st.push_back(i);
     }
     rep(i, 1, sz(a)) t[i][1] =
-      (t[i][1] | t[i - 1][1]) & -(t[i][0] & -t[i][0]);
+        (t[i][1] | t[i - 1][1]) & -(t[i][0] & -t[i][0]);
   }
-  int query_idx(int l, int r) { // [l, r]
+  int query_idx(int l, int r) {  // [l, r]
     if (int j = t[l][0] ^ t[r][0]; j) {
       j = t[l][1] & t[r][1] & -(1 << __lg(j));
       if (int k = t[l][1] ^ j; k)
