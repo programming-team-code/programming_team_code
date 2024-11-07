08bbf9aca310a1192d05f45af3a4b79dc5cf5470
Luke Videckis <lukevideckis@gmail.com>
2024-11-07 09:14:28 -0600

    add par array

--- library/trees/centroid_decomp.hpp
+++ library/trees/centroid_decomp.hpp
@@ -9,21 +9,21 @@
 template<class F> struct centroid {
   vector<vi> adj;
   F f;
-  vi sub_sz;
+  vi siz, par;
   centroid(const vector<vi>& a_adj, F a_f):
-    adj(a_adj), f(a_f), sub_sz(sz(adj), -1) {
-    rep(i, 0, sz(adj)) if (sub_sz[i] == -1) dfs(i);
+    adj(a_adj), f(a_f), siz(sz(adj), -1), par(siz) {
+    rep(i, 0, sz(adj)) if (siz[i] == -1) dfs(i);
   }
   void calc_sz(int v, int p) {
-    sub_sz[v] = 1;
+    siz[v] = 1;
     for (int u : adj[v])
-      if (u != p) calc_sz(u, v), sub_sz[v] += sub_sz[u];
+      if (u != p) calc_sz(u, v), siz[v] += siz[u];
   }
-  void dfs(int v) {
+  int dfs(int v) {
     calc_sz(v, -1);
-    for (int p = -1, sz_root = sub_sz[v];;) {
+    for (int p = -1, sz_root = siz[v];;) {
       auto big_ch = find_if(all(adj[v]), [&](int u) {
-        return u != p && 2 * sub_sz[u] > sz_root;
+        return u != p && 2 * siz[u] > sz_root;
       });
       if (big_ch == end(adj[v])) break;
       p = v, v = *big_ch;
@@ -32,7 +32,8 @@ template<class F> struct centroid {
     for (int u : adj[v]) {
       iter_swap(find(all(adj[u]), v), rbegin(adj[u]));
       adj[u].pop_back();
-      dfs(u);
+      par[dfs(u)] = v;
     }
+    return v;
   }
 };
