a77a224cad9f48797a35b027266a79b431fef0c4
Luke Videckis <lukevideckis@gmail.com>
2024-11-07 09:34:29 -0600

    golf

--- library/trees/centroid_decomp.hpp
+++ library/trees/centroid_decomp.hpp
@@ -9,21 +9,21 @@
 template<class F> struct centroid {
   vector<vi> adj;
   F f;
-  vi sub_sz;
+  vi siz;
   centroid(const vector<vi>& a_adj, F a_f):
-    adj(a_adj), f(a_f), sub_sz(sz(adj), -1) {
-    rep(i, 0, sz(adj)) if (sub_sz[i] == -1) dfs(i, -1);
+    adj(a_adj), f(a_f), siz(sz(adj), -1) {
+    rep(i, 0, sz(adj)) if (siz[i] == -1) dfs(i, -1);
   }
   void calc_sz(int v, int p) {
-    sub_sz[v] = 1;
+    siz[v] = 1;
     for (int u : adj[v])
-      if (u != p) calc_sz(u, v), sub_sz[v] += sub_sz[u];
+      if (u != p) calc_sz(u, v), siz[v] += siz[u];
   }
   void dfs(int v, int p) {
     calc_sz(v, -1);
-    for (int w = -1, sz_root = sub_sz[v];;) {
+    for (int w = -1, sz_root = siz[v];;) {
       auto big_ch = find_if(all(adj[v]), [&](int u) {
-        return u != w && 2 * sub_sz[u] > sz_root;
+        return u != w && 2 * siz[u] > sz_root;
       });
       if (big_ch == end(adj[v])) break;
       w = v, v = *big_ch;
