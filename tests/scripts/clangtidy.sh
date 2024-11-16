7a7aa3cc2eb60931002e07953e529659c92d1231
Luke Videckis <lukevideckis@gmail.com>
2024-11-16 10:42:46 -0600

    trying this

--- tests/scripts/clangtidy.sh
+++ tests/scripts/clangtidy.sh
@@ -6,5 +6,5 @@ git submodule init
 git submodule update
 
 find library_checker_aizu_tests/ -type f -name "*.test.cpp" |
-	parallel clang-tidy --config-file=.config/.clang-tidy {} -- -std=c++23 ||
+	parallel clang-tidy --config-file=.config/.clang-tidy {} -- -std=gnu++20 ||
 	exit 1
