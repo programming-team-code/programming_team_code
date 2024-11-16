ca6743a0c99dfa8e3fa244607ab9f302fdbdde71
Luke Videckis <lukevideckis@gmail.com>
2024-11-16 11:37:21 -0600

    actually just remove these

--- tests/scripts/compile_commented_snippets.sh
+++ tests/scripts/compile_commented_snippets.sh
@@ -11,7 +11,7 @@ git submodule update
 {
 	cat library_checker_aizu_tests/template.hpp
 	echo "const ll mod = (119 << 23) + 1, root = 62;"
-	find ../library/ -type f -name "*.hpp" | grep --invert-match --file=.config/.code_snippet_excluded_file_list | sort | sed 's/^/#include "/; s/$/"/' | cpp -std=c17 -nostdinc -C -P | grep --invert-match --extended-regexp "const int mod = |const ll mod = "
+	find ../library/ -type f -name "*.hpp" | grep --invert-match --file=.config/.code_snippet_excluded_file_list | sort | sed 's/^/#include "/; s/$/"/' | cpp -nostdinc -C -P | grep --invert-match --extended-regexp "const int mod = |const ll mod = "
 	echo "int main() {"
 	echo "vi a,b,subset;"
 	echo "vl left,bottom,s_vec,t_vec;"
