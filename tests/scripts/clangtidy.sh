704864bfaab26441a4b0ea4740d3df04554831b6
Luke Videckis <lukevideckis@gmail.com>
2024-11-16 10:54:01 -0600

    check version

--- tests/scripts/clangtidy.sh
+++ tests/scripts/clangtidy.sh
@@ -5,6 +5,8 @@ shopt -s globstar
 git submodule init
 git submodule update
 
+clang-tidy --version
+
 find library_checker_aizu_tests/ -type f -name "*.test.cpp" |
-	parallel clang-tidy --config-file=.config/.clang-tidy {} -- -std=gnu++20 ||
+	parallel clang-tidy --config-file=.config/.clang-tidy {} -- -std=c++20 ||
 	exit 1
