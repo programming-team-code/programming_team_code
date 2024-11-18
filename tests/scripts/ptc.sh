9d7c6465daa5db4d571199b6a7e19742a7cd87c4
Luke Videckis <lukevideckis@gmail.com>
2024-11-18 12:25:25 -0600

    remove a bunch of stuff

--- tests/scripts/ptc.sh
+++ tests/scripts/ptc.sh
@@ -27,8 +27,6 @@ rm ../library/monotonic_stack/max_rect_histogram.hpp
 rm ../library/monotonic_stack/cartesian_binary_tree_after.png
 rm ../library/monotonic_stack/mono_stack_ri.png
 rm ../library/strings/manacher/longest_palindrome_query.hpp
-rm ../library/strings/suffix_array/burrows_wheeler.hpp
-rm -rf ../library/strings/suffix_array/lcp_interval_tree/
 rm -rf ../library/trees/centroid_decomp_uncommon/
 rm -rf ../library/trees/edge_centroid_decomp_uncommon/
 rm -rf ../library/trees/ladder_decomposition/ladder_decomposition.hpp
