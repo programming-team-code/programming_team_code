9be890962efdb3ec7f9e88acda65f4829d39ef68
Luke Videckis <lrvideckis@gmail.com>
2024-11-18 22:22:41 -0600

    Wavelet matrix (#100)
    * now compiles
    
    * [auto-verifier] verify commit 64763eef61401e9c952f37d8aea163521dd81414
    
    * add another test
    
    * [auto-verifier] verify commit c7ccaf30313d41935aa83e791845c943cd911f5e
    
    * final touches
    
    * [auto-verifier] verify commit 5ad3ffbd4c48ea974f6cb37061c1270c6f2fc2c9
    
    * add complexity
    
    * fix
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ library/data_structures/seg_tree_uncommon/wavelet_count_less.hpp
@@ -0,0 +1,16 @@
+#pragma once
+//! Requires ub >= 0
+//! count of i in [l..r) such that a[i] < ub
+//! @time O(log(max_val))
+//! @space O(1)
+int count(int l, int r, ll ub) {
+  int res = 0;
+  for (int h = sz(bv); h--;) {
+    int l0 = bv[h].cnt0(l), r0 = bv[h].cnt0(r);
+    if ((~ub >> h) & 1) l = l0, r = r0;
+    else
+      res += r0 - l0, l += bv[h].cnt0(n) - l0,
+        r += bv[h].cnt0(n) - r0;
+  }
+  return res;
+}
--- tests/library_checker_aizu_tests/handmade_tests/merge_st_and_wavelet.test.cpp
+++ tests/library_checker_aizu_tests/handmade_tests/merge_st_and_wavelet.test.cpp
@@ -3,11 +3,12 @@
 #include "../template.hpp"
 #include "../../../library/contest/random.hpp"
 #include "../../../library/data_structures/seg_tree_uncommon/merge_sort_tree.hpp"
+#include "../../../library/data_structures/seg_tree_uncommon/wavelet_matrix.hpp"
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   // brute force small cases
   for (int n = 0; n <= 25; n++) {
-    for (int tests = 3; tests--;) {
+    for (int tests = 10; tests--;) {
       int minn = rnd<int>(-1000, 1000);
       int maxn = rnd<int>(-1000, 1000);
       if (minn > maxn) swap(minn, maxn);
@@ -15,14 +16,20 @@ int main() {
       generate(begin(arr), end(arr),
         [&]() { return rnd<int>(minn, maxn); });
       merge_sort_tree mst(arr);
+      vector<ll> arr_shifted(n);
+      rep(i, 0, n) arr_shifted[i] = arr[i] - minn;
+      wavelet_matrix wm(arr_shifted, maxn - minn);
       for (int queries = 30; queries--;) {
-        int x = rnd<int>(-1000, 1000);
-        int y = rnd<int>(-1000, 1000);
+        int x = rnd<int>(minn, maxn);
+        int y = rnd<int>(minn, maxn);
         if (x > y) swap(x, y);
         for (int l = 0; l <= n; l++) {
           int cnt = 0;
           for (int r = l; r <= n; r++) {
             assert(mst.query(l, r, x, y) == cnt);
+            assert(wm.count(l, r, y - minn) -
+                wm.count(l, r, x - minn) ==
+              cnt);
             if (r < n && x <= arr[r] && arr[r] < y) cnt++;
           }
         }
