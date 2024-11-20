fc7a466c5bedfcae25960489a6ce413bd1485a7f
Luke Videckis <lrvideckis@gmail.com>
2024-11-19 22:43:54 -0600

    Add lcp (#103)
    * add lcp now
    
    * fix
    
    * fix

--- library/strings/suffix_array/suffix_array_short.hpp
+++ library/strings/suffix_array/suffix_array_short.hpp
@@ -1,14 +1,14 @@
 #pragma once
 //! https://github.com/atcoder/ac-library/blob/master/atcoder/string.hpp
 //! @code
-//!   auto [sa2, sa_inv2] = sa_short(s);
+//!   auto [sa2, sa_inv2, lcp2] = sa_short(s);
 //! @endcode
 //! runs in ~1.5s for 5e5
 //! @time O(n * log^2(n))
 //! @space O(n)
-template<class T> array<vi, 2> sa_short(const T& s) {
+template<class T> array<vi, 3> sa_short(const T& s) {
   int n = sz(s);
-  vi sa(n), sa_inv(all(s));
+  vi sa(n), sa_inv(all(s)), lcp(n - 1);
   iota(all(sa), 0);
   for (int k = 1; k <= n; k *= 2) {
     vi x(sa_inv);
@@ -20,5 +20,14 @@ template<class T> array<vi, 2> sa_short(const T& s) {
     rep(i, 1, n) sa_inv[sa[i]] =
       sa_inv[sa[i - 1]] + (proj(sa[i - 1]) != proj(sa[i]));
   }
-  return {sa, sa_inv};
+  int sz = 0;
+  rep(i, 0, n) {
+    if (sz > 0) sz--;
+    if (sa_inv[i] == 0) continue;
+    for (int j = sa[sa_inv[i] - 1];
+         max(i, j) + sz < n && s[i + sz] == s[j + sz];)
+      sz++;
+    lcp[sa_inv[i] - 1] = sz;
+  }
+  return {sa, sa_inv, lcp};
 }
--- tests/library_checker_aizu_tests/strings/lcp_array.test.cpp
+++ tests/library_checker_aizu_tests/strings/lcp_array.test.cpp
@@ -1,7 +1,11 @@
 #define PROBLEM \
   "https://judge.yosupo.jp/problem/number_of_substrings"
+// just because with debug mode, test runs in 30s, and
+// there's like 30 tests
+#undef _GLIBCXX_DEBUG
 #include "../template.hpp"
 #include "../../../library/strings/suffix_array/suffix_array.hpp"
+#include "../../../library/strings/suffix_array/suffix_array_short.hpp"
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   {
@@ -16,6 +20,10 @@ int main() {
   cin >> s;
   int n = sz(s);
   auto [sa, sa_inv, lcp] = get_sa(s, 256);
+  auto [sa1, sa_inv1, lcp1] = sa_short(s);
+  assert(sa == sa1);
+  assert(sa_inv == sa_inv1);
+  assert(lcp == lcp1);
   assert(sz(lcp) == n - 1);
   cout << 1LL * n * (n + 1) / 2 -
       accumulate(begin(lcp), end(lcp), 0LL)
