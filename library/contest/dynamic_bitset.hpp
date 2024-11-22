b64e919a982f67666f1b1216831f1e9a5f6562a8
Luke Videckis <lukevideckis@gmail.com>
2024-11-22 10:40:54 -0600

    bitset

---
+++ library/contest/dynamic_bitset.hpp
@@ -0,0 +1,24 @@
+//! https://codeforces.com/blog/entry/129454
+//! https://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/a09090.html
+#include <tr2/dynamic_bitset>
+using namespace tr2;
+void f() {
+  dynamic_bitset<> bs(65);
+  assert(bs.bits_per_block == 64);
+  assert(bs.num_blocks() == 2);
+  assert(bs.none());
+  bs[1] = bs[3] = 1;
+  assert(bs.any());
+  assert(bs.count() == 2);
+  assert(ssize(bs) == 65);
+  bs |= bs & bs;
+  assert(
+    bs == bs && !(bs != bs) && !(bs < bs) && bs <= bs);
+  bs.resize(10);
+  bs.push_back(0);
+  cerr << bs << endl;
+  for (int i = bs.find_first(); i != ssize(bs);
+       i = bs.find_next(i))
+    cerr << i << endl;
+  bs -= bs; // bs &= ~bs;
+}
---
+++ tests/library_checker_aizu_tests/handmade_tests/dynamic_bitset.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/problems/ITP1_1_A"
+#include "../template.hpp"
+#include "../../../library/contest/dynamic_bitset.hpp"
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  f();
+  cout << "Hello World\n";
+  return 0;
+}
