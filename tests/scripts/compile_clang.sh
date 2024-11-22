72262c7198d401631076d3b808fba60d49ce9b06
Luke Videckis <lrvideckis@gmail.com>
2024-11-22 10:47:44 -0600

    Bitset (#104)
    * bitset
    
    * [auto-verifier] verify commit b64e919a982f67666f1b1216831f1e9a5f6562a8
    
    * fixes
    
    * fix this too
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- tests/scripts/compile_clang.sh
+++ tests/scripts/compile_clang.sh
@@ -2,6 +2,9 @@
 # ** glob now searches any number of levels
 shopt -s globstar
 
+# doesn't compile with clang
+rm library_checker_aizu_tests/handmade_tests/dynamic_bitset.test.cpp
+
 git submodule init
 git submodule update
 
