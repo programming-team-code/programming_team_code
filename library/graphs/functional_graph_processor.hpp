66760384ba5e412eb8c5cac9065aeac32a78b699
Luke Videckis <lrvideckis@gmail.com>
2024-11-27 14:38:43 -0600

    Functional graph (#106)
    * adding function graph
    
    * [auto-verifier] verify commit 23f3219f2695b98b3bdedca6418b5e2b980e8be1
    
    * condense
    
    * [auto-verifier] verify commit 42e88aed64265f19725f25c5237d9fdf01165eff
    
    * fix shadow
    
    * [auto-verifier] verify commit 4ea5606a8e5d471eab35dd64b8f753f598bfd9cc
    
    * add docs now
    
    * fix
    
    * don't add in PDF
    
    * [auto-verifier] verify commit 990288844356a75f13151ddac576d8d4861e8413
    
    * [auto-verifier] verify commit df23d07041e9aeb5ce8f874d9fd2abe5182f848e
    
    * fixes
    
    * fix conflicting name
    
    * fix
    
    * fix
    
    * [auto-verifier] verify commit f8314350889b17f265d916a636dabeb2b875ddeb
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ library/graphs/functional_graph_processor.hpp
@@ -0,0 +1,52 @@
+#pragma once
+//! https://github.com/Aeren1564/Algorithms/blob/master/Algorithm_Implementations_Cpp/Graph_Theory/Special_Graphs/functional_graph_processor.sublime-snippet
+//! @code
+//!   // 0 <= a[i] < n
+//!   auto [t2, cycle] = func_graph(a);
+//!   if (auto id = t2[v].cyc_pos)
+//!     assert(v == cycle[id->first][id->second]);
+//! @endcode
+//! t[v].root_of = first reachable node in a cycle
+//! t[v].childs = forest of reversed edges not in cycles
+//! @time O(n)
+//! @space O(n)
+struct func_graph {
+  struct node {
+    int root_of;
+    optional<pii> cyc_pos;
+    vi childs;
+  };
+  vector<node> t;
+  vector<vi> cycle;
+  func_graph(const vi &a): t(sz(a)) {
+    vi state(sz(a));
+    rep(i, 0, sz(a)) {
+      if (state[i] == 0) {
+        int v = i;
+        while (state[v] == 0) {
+          state[v] = 1;
+          v = a[v];
+        }
+        if (state[v] == 1) {
+          cycle.emplace_back();
+          while (state[v] == 1) {
+            t[v].root_of = v;
+            t[v].cyc_pos = {
+              sz(cycle) - 1, sz(cycle.back())};
+            cycle.back().push_back(v);
+            state[v] = 2;
+            v = a[v];
+          }
+        }
+        int curr_root_of = t[v].root_of;
+        v = i;
+        while (state[v] == 1) {
+          t[v].root_of = curr_root_of;
+          t[a[v]].childs.push_back(v);
+          state[v] = 2;
+          v = a[v];
+        }
+      }
+    }
+  }
+};
