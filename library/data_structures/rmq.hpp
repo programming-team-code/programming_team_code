754de206af3617d39ce77a9c4a6bc377c6fc6413
Luke Videckis <lukevideckis@gmail.com>
2024-12-04 14:36:55 -0600

    switch

--- library/data_structures/rmq.hpp
+++ library/data_structures/rmq.hpp
@@ -1,9 +1,9 @@
 #pragma once
 //! @code
-//!   RMQ rmq1(a, ranges::min);
-//!   RMQ rmq2(a, [&](auto& x, auto& y) {
-//!     return min(x, y);
-//!   });
+//!   RMQ<int, ranges::min> rmq1(a);
+//!   RMQ<int,([&](auto& x, auto& y) {
+//!     return x > y ? x : y;
+//!   })> rmq2(a);
 //!   vector<RMQ<int, function<int(int, int)>>>
 //!     rmqs(3, {{}, NULL});
 //!   rmqs[1] = {a, ranges::min};
@@ -11,19 +11,18 @@
 //! @time O(nlogn + q)
 //! @space O(nlogn)
 // NOLINTNEXTLINE(readability-identifier-naming)
-template<class T, class F> struct RMQ {
+template<class T, auto OP> struct RMQ {
   vector<vector<T>> dp;
-  F op;
-  RMQ(const vector<T>& a, F a_op): dp(1, a), op(a_op) {
+  RMQ(const vector<T>& a): dp(1, a) {
     for (int i = 0; (2 << i) <= sz(a); i++) {
       dp.emplace_back(sz(a) - (2 << i) + 1);
       ranges::transform(dp[i], dp[i] | views::drop(1 << i),
-        begin(dp[i + 1]), op);
+        begin(dp[i + 1]), OP);
     }
   }
   T query(int l, int r) { // [l, r)
     assert(l < r);
     int lg = __lg(r - l);
-    return op(dp[lg][l], dp[lg][r - (1 << lg)]);
+    return OP(dp[lg][l], dp[lg][r - (1 << lg)]);
   }
 };
--- library/data_structures/rmq_inc.hpp
+++ library/data_structures/rmq_inc.hpp
@@ -10,19 +10,18 @@
 //! @endcode
 //! @time O(nlogn + q)
 //! @space O(nlogn)
-template<class T, class F> struct rmq_inc {
+template<class T, auto OP> struct rmq_inc {
   vector<vector<T>> dp;
-  F op;
-  rmq_inc(const vector<T>& a, F a_op): dp(1, a), op(a_op) {
+  rmq_inc(const vector<T>& a): dp(1, a) {
     for (int i = 0; (2 << i) <= sz(a); i++) {
       dp.emplace_back(sz(a) - (2 << i) + 1);
       ranges::transform(dp[i], dp[i] | views::drop(1 << i),
-        begin(dp[i + 1]), op);
+        begin(dp[i + 1]), OP);
     }
   }
   T query(int l, int r) { // [l, r]
     assert(l <= r);
     int lg = __lg(r - l + 1);
-    return op(dp[lg][l], dp[lg][r - (1 << lg) + 1]);
+    return OP(dp[lg][l], dp[lg][r - (1 << lg) + 1]);
   }
 };
