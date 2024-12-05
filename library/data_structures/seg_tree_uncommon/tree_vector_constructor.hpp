97f0f2f80946810d26e2670e93ec1bad3942e821
Luke Videckis <lrvideckis@gmail.com>
2024-12-05 14:57:04 -0600

    Update inc seg (#114)
    * update
    
    * update link
    
    * add back check
    
    * [auto-verifier] verify commit 3ebf11cc5570d384444d8586bdb6938c4f47543d
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/data_structures/seg_tree_uncommon/tree_vector_constructor.hpp
+++ library/data_structures/seg_tree_uncommon/tree_vector_constructor.hpp
@@ -1,7 +1,7 @@
 #pragma once
-tree(const vector<T>& a, T unit, F f):
-  n(sz(a)), unit(unit), f(f), s(2 * n) {
+tree(const vector<T>& a, T unit, F op):
+  n(sz(a)), unit(unit), op(op), s(2 * n) {
   rep(i, 0, n) s[i + n] = a[i];
   for (int i = n - 1; i > 0; i--)
-    s[i] = f(s[2 * i], s[2 * i + 1]);
+    s[i] = op(s[2 * i], s[2 * i + 1]);
 }
