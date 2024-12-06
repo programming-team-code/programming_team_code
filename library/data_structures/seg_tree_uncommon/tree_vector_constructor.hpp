58354661b899532bedd6532322fa1c6bb17ea5ac
Luke Videckis <lukevideckis@gmail.com>
2024-12-05 21:54:55 -0600

    update

--- library/data_structures/seg_tree_uncommon/tree_vector_constructor.hpp
+++ library/data_structures/seg_tree_uncommon/tree_vector_constructor.hpp
@@ -1,7 +1,7 @@
 #pragma once
 tree(const vector<T>& a, T unit, F op):
   n(sz(a)), unit(unit), op(op), s(2 * n) {
-  rep(i, 0, n) s[i + n] = a[i];
+  ranges::copy(a, begin(s) + n);
   for (int i = n - 1; i > 0; i--)
     s[i] = op(s[2 * i], s[2 * i + 1]);
 }
