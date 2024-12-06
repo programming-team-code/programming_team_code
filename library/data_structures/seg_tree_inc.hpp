03ee09acd8e371f99948576084bb598296cee21c
Luke Videckis <lrvideckis@gmail.com>
2024-12-05 22:01:03 -0600

    Speedup (#115)
    * update
    
    * [auto-verifier] verify commit 58354661b899532bedd6532322fa1c6bb17ea5ac
    
    * fix cppcheck
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/data_structures/seg_tree_inc.hpp
+++ library/data_structures/seg_tree_inc.hpp
@@ -8,13 +8,17 @@
 //! @endcode
 //! @time O(n + q log n)
 //! @space O(n)
+int nxt(int& l, int r) {
+  int lg = __lg(min(l & -l, r - l + 1));
+  return exchange(l, l + (1 << lg)) >> lg;
+}
 template<class T, class F> struct tree_inc {
   int n;
   F op;
   vector<T> s;
   tree_inc(const vector<T>& a, F op):
     n(sz(a)), op(op), s(2 * n) {
-    rep(i, 0, n) s[i + n] = a[i];
+    ranges::copy(a, begin(s) + n);
     for (int i = n - 1; i > 0; i--)
       s[i] = op(s[2 * i], s[2 * i + 1]);
   }
@@ -22,13 +26,9 @@ template<class T, class F> struct tree_inc {
     for (s[i += n] = val; i /= 2;)
       s[i] = op(s[2 * i], s[2 * i + 1]);
   }
-  T nxt(int& l, int r) {
-    int lg = __lg(min(l & -l, r - l + 1));
-    return s[exchange(l, l + (1 << lg)) >> lg];
-  }
   T query(int l, int r) { // [l, r]
-    T res = nxt(l += n, r += n);
-    while (l <= r) res = op(res, nxt(l, r));
+    T res = s[nxt(l += n, r += n)];
+    while (l <= r) res = op(res, s[nxt(l, r)]);
     return res;
   }
 };
