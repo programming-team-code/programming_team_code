72d7e2b4c978ac67fc60972a136d033ce01335df
Luke Videckis <lukevideckis@gmail.com>
2024-12-06 01:10:23 -0600

    remove linear constructor to be shorter

--- library/data_structures/seg_tree_inc.hpp
+++ library/data_structures/seg_tree_inc.hpp
@@ -5,6 +5,7 @@
 //!   tree_inc st4(a, [&](int x, int y) -> int {
 //!     return min(x, y);
 //!   });
+//!   rep(i, 0, n) st3.update(i, a[i]);
 //! @endcode
 //! @time O(n + q log n)
 //! @space O(n)
@@ -16,12 +17,7 @@ template<class T, class F> struct tree_inc {
   int n;
   F op;
   vector<T> s;
-  tree_inc(const vector<T>& a, F op):
-    n(sz(a)), op(op), s(2 * n) {
-    ranges::copy(a, begin(s) + n);
-    for (int i = n - 1; i > 0; i--)
-      s[i] = op(s[2 * i], s[2 * i + 1]);
-  }
+  tree_inc(int n, F op): n(n), op(op), s(2 * n) {}
   void update(int i, T val) {
     for (s[i += n] = val; i /= 2;)
       s[i] = op(s[2 * i], s[2 * i + 1]);
--- library/data_structures/seg_tree_uncommon/tree_vector_constructor.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-tree(const vector<T>& a, T unit, F op):
-  n(sz(a)), unit(unit), op(op), s(2 * n) {
-  ranges::copy(a, begin(s) + n);
-  for (int i = n - 1; i > 0; i--)
-    s[i] = op(s[2 * i], s[2 * i + 1]);
-}
