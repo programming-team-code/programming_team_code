f5c359b494ad16619ca68670e4a0bbd812ce24e2
Luke Videckis <lrvideckis@gmail.com>
2024-12-15 09:01:54 -0600

    Another scc golf (#123)
    * golf
    
    * [auto-verifier] verify commit 596f983898189325d31ead59997d102a949f8862
    
    * golf
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/graphs/bridges_cuts/cuts.hpp
+++ library/graphs/bridges_cuts/cuts.hpp
@@ -21,31 +21,22 @@
 //! @time O(n + m)
 //! @space O(n + m)
 auto cuts(const auto& adj, int m) {
-  int n = sz(adj), num_bccs = 0, timer = 0;
-  vi bcc_id(m, -1), is_cut(n), tin(n), st;
-  auto dfs = [&](auto&& self, int v, int p_id) -> int {
-    int low = tin[v] = ++timer, deg = 0;
-    for (auto [u, e_id] : adj[v]) {
+  int n = sz(adj), num_bccs = 0, q = 0, s = 0;
+  vi bcc_id(m, -1), is_cut(n), tin(n), st(m);
+  auto dfs = [&](auto&& self, int v, int p) -> int {
+    int low = tin[v] = ++q;
+    for (auto [u, e] : adj[v]) {
       assert(v != u);
-      if (e_id == p_id) continue;
-      if (!tin[u]) {
-        int siz = sz(st);
-        st.push_back(e_id);
-        int low_ch = self(self, u, e_id);
-        if (low_ch >= tin[v]) {
-          is_cut[v] = 1;
-          rep(i, siz, sz(st)) bcc_id[st[i]] = num_bccs;
-          st.resize(siz);
-          num_bccs++;
-        }
-        low = min(low, low_ch);
-        deg++;
-      } else if (tin[u] < tin[v]) {
-        st.push_back(e_id);
-        low = min(low, tin[u]);
+      if (e == p) continue;
+      if (tin[u] < tin[v]) st[s++] = e;
+      int lu = -1;
+      low = min(low, tin[u] ?: (lu = self(self, u, e)));
+      if (lu >= tin[v]) {
+        is_cut[v] = p >= 0 || tin[v] + 1 < tin[u];
+        while (bcc_id[e] < 0) bcc_id[st[--s]] = num_bccs;
+        num_bccs++;
       }
     }
-    if (p_id == -1) is_cut[v] = (deg > 1);
     return low;
   };
   rep(i, 0, n) if (!tin[i]) dfs(dfs, i, -1);
