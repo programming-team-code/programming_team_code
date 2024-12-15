191ff65cd45dbf9a059ed3a93473593e71dbdb15
Luke Videckis <lrvideckis@gmail.com>
2024-12-15 17:01:11 -0600

    Fixes (#124)
    * remove slow test LOL
    
    * [auto-verifier] verify commit 3573d11f865bb6b60aaffbe8a4e3573686b6fc89
    
    * trying this
    
    * [auto-verifier] verify commit 470a1fd25e9963714871633a641fdf4c2b6086b3
    
    * trying this
    
    * [auto-verifier] verify commit e1d14907f7a4b2392c4e3afd6c163d329e1de7ff
    
    * update old var names
    
    * [auto-verifier] verify commit 247368a566148f4bf77b3d4eebdec78485669c76
    
    * update
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/graphs/functional_graph_processor.hpp
+++ library/graphs/functional_graph_processor.hpp
@@ -2,8 +2,8 @@
 //! https://github.com/Aeren1564/Algorithms/blob/master/Algorithm_Implementations_Cpp/Graph_Theory/Special_Graphs/functional_graph_processor.sublime-snippet
 //! @code
 //!   // 0 <= a[i] < n
-//!   auto [t2, cycle] = func_graph(a);
-//!   auto [cyc_id, cyc_pos] = t2[v].root_of;
+//!   auto [t, cycle] = func_graph(a);
+//!   auto [cyc_id, cyc_pos] = t[v].root_of;
 //!   int root = cycle[cyc_id][cyc_pos];
 //!   bool is_on_cycle = (v == root);
 //! @endcode
