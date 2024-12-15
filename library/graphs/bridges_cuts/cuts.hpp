187baf1f4f4bf504f0bc14c28ece263a443401de
Luke Videckis <lukevideckis@gmail.com>
2024-12-15 08:59:05 -0600

    golf

--- library/graphs/bridges_cuts/cuts.hpp
+++ library/graphs/bridges_cuts/cuts.hpp
@@ -24,26 +24,19 @@ auto cuts(const auto& adj, int m) {
   int n = sz(adj), num_bccs = 0, q = 0, s = 0;
   vi bcc_id(m, -1), is_cut(n), tin(n), st(m);
   auto dfs = [&](auto&& self, int v, int p) -> int {
-    int low = tin[v] = ++q, deg = 0;
+    int low = tin[v] = ++q;
     for (auto [u, e] : adj[v]) {
       assert(v != u);
       if (e == p) continue;
-      if (!tin[u]) {
-        st[s++] = e;
-        int low_ch = self(self, u, e);
-        if (low_ch >= tin[v]) {
-          is_cut[v] = 1;
-          while (bcc_id[e] < 0) bcc_id[st[--s]] = num_bccs;
-          num_bccs++;
-        }
-        low = min(low, low_ch);
-        deg++;
-      } else if (tin[u] < tin[v]) {
-        st[s++] = e;
-        low = min(low, tin[u]);
+      if (tin[u] < tin[v]) st[s++] = e;
+      int lu = -1;
+      low = min(low, tin[u] ?: (lu = self(self, u, e)));
+      if (lu >= tin[v]) {
+        is_cut[v] = p >= 0 || tin[v] + 1 < tin[u];
+        while (bcc_id[e] < 0) bcc_id[st[--s]] = num_bccs;
+        num_bccs++;
       }
     }
-    if (p == -1) is_cut[v] = (deg > 1);
     return low;
   };
   rep(i, 0, n) if (!tin[i]) dfs(dfs, i, -1);
