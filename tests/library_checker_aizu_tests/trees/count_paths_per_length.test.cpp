bc0a0b7304b4481cf86f311ec43bcfc0ab877086
Luke Videckis <lukevideckis@gmail.com>
2024-12-17 08:13:52 -0600

    seeing if this is enough

--- tests/library_checker_aizu_tests/trees/count_paths_per_length.test.cpp
+++ tests/library_checker_aizu_tests/trees/count_paths_per_length.test.cpp
@@ -6,7 +6,7 @@ int main() {
   cin.tie(0)->sync_with_stdio(0);
   int n;
   cin >> n;
-  vector<vector<int>> adj(n);
+  vector<basic_string<int>> adj(n);
   for (int i = 0; i < n - 1; i++) {
     int u, v;
     cin >> u >> v;
