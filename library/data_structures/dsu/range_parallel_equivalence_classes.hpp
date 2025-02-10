86f5ee9dbb45e61a0ccb9113a42da3f64d205981
Luke Videckis <lrvideckis@gmail.com>
2025-02-10 13:38:04 -0700

    format

--- library/data_structures/dsu/range_parallel_equivalence_classes.hpp
+++ library/data_structures/dsu/range_parallel_equivalence_classes.hpp
@@ -6,10 +6,12 @@
 //! @space O(n + q)
 UF get_rp_dsu(const vector<array<int, 3>>& rests, int n) {
   vector<basic_string<array<int, 2>>> rests_by_len(n + 1);
-  for (auto [l1, l2, len] : rests) rests_by_len[len] += {l1, l2};
+  for (auto [l1, l2, len] : rests)
+    rests_by_len[len] += {l1, l2};
   UF uf(n);
   for (int len = n; len > 0; len--)
     for (auto [l1, l2] : rests_by_len[len])
-      if (uf.join(l1, l2)) rests_by_len[len - 1] += {l1 + 1, l2 + 1};
+      if (uf.join(l1, l2))
+        rests_by_len[len - 1] += {l1 + 1, l2 + 1};
   return uf;
 }
