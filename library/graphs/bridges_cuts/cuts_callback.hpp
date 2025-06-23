de0d02e8837f3e1dde6f155d5878f3c35b96f30a
Luke Videckis <lrvideckis@gmail.com>
2025-06-23 02:28:30 -0600

    golf and format

--- library/graphs/bridges_cuts/cuts_callback.hpp
+++ library/graphs/bridges_cuts/cuts_callback.hpp
@@ -14,48 +14,26 @@
 //!     adj[u] += {v, i};
 //!     adj[v] += {u, i};
 //!   }
-//!   auto [num_bccs, bcc_id, is_cut] = cuts(adj, m);
+//!   cuts_callback(adj, [&](int v, const vi& nodes_bcc) {
+//!   });
 //! @endcode
 //! @time O(n + m)
 //! @space O(n + m)
-/*
-auto cuts(const auto& adj, int m) {
-  int n = sz(adj), num_bccs = 0, q = 0, s = 0;
-  vi bcc_id(m, -1), is_cut(n), tin(n), st(m);
-  auto dfs = [&](auto&& self, int v, int p) -> int {
-    int low = tin[v] = ++q;
-    for (auto [u, e] : adj[v]) {
-      assert(v != u);
-      if (e == p) continue;
-      if (tin[u] < tin[v]) st[s++] = e;
-      int lu = -1;
-      low = min(low, tin[u] ?: (lu = self(self, u, e)));
-      if (lu >= tin[v]) {
-        is_cut[v] = p >= 0 || tin[v] + 1 < tin[u];
-        while (bcc_id[e] < 0) bcc_id[st[--s]] = num_bccs;
-        num_bccs++;
-      }
-    }
-    return low;
-  };
-  rep(i, 0, n) if (!tin[i]) dfs(dfs, i, -1);
-  return tuple{num_bccs, bcc_id, is_cut};
-}
-*/
 void cuts_callback(const auto& adj, auto f) {
-  int n = ssize(adj), q = 0, s = 0;
-  vector<int> tin(n), st(n);
+  int n = sz(adj), q = 0, s = 0;
+  vi tin(n), st(n);
   auto dfs = [&](auto&& self, int v, int p) -> int {
     int low = tin[v] = ++q;
-    for (int u : adj[v]) if (u != p) {
-      int siz = s, lu = 0;
-      low = min(low, tin[u] ?: (lu = self(self, st[s++] = u, v)));
-      if (lu >= tin[v]) {
-        f(v, vector<int>(begin(st) + siz, begin(st) + s));
-        s = siz;
+    for (int u : adj[v])
+      if (u != p) {
+        int siz = s, lu = 0;
+        low = min(low,
+          tin[u] ?: (lu = self(self, st[s++] = u, v)));
+        if (lu >= tin[v])
+          f(st[s] = v, vi(siz + all(st) - (n - s))),
+            s = siz;
       }
-    }
     return low;
   };
-  for (int i = 0; i <n; i++) if (!tin[i]) dfs(dfs, i, i);
+  rep(i, 0, n) if (!tin[i]) dfs(dfs, i, i);
 }
