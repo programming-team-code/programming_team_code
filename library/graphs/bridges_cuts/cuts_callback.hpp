fa49926b000502ac8ee270f5e699677d95445802
Luke Videckis <lrvideckis@gmail.com>
2025-06-23 04:31:17 -0600

    more golf

--- library/graphs/bridges_cuts/cuts_callback.hpp
+++ library/graphs/bridges_cuts/cuts_callback.hpp
@@ -16,17 +16,17 @@ void cuts_callback(const auto& adj, auto f) {
   int n = sz(adj), q = 0, s = 0;
   vi tin(n), st(n);
   auto dfs = [&](auto&& self, int v) -> int {
-    int low = tin[v] = ++q;
+    int l = tin[v] = ++q;
     for (int u : adj[v]) {
       int siz = s, lu = 0;
-      low = min(low, tin[u] ?: (lu = self(self, st[s++] = u)));
+      l = min(l, tin[u] ?: (lu = self(self, st[s++] = u)));
       if (lu >= tin[v]) {
         st[s++] = v;
         f(vi(siz + all(st) - n + s));
         s = siz;
       }
     }
-    return low;
+    return l;
   };
   rep(i, 0, n) if (!tin[i]) dfs(dfs, i);
 }
