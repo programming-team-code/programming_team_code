cec9d7bbf417971e79068e026388bdee82999dd9
Luke Videckis <lrvideckis@gmail.com>
2025-06-23 04:33:28 -0600

    golf (#136)
    * Update cuts_callback.hpp
    
    * [auto-verifier] verify commit cbaaa72a60fa26aade67b92e55dafad1aa8ee2f5
    
    * more golf
    
    * [auto-verifier] verify commit 408c4e1201db6497ef42bf5885575606c2e61d19
    
    * more golf
    
    * [auto-verifier] verify commit fa49926b000502ac8ee270f5e699677d95445802
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/graphs/bridges_cuts/cuts_callback.hpp
+++ library/graphs/bridges_cuts/cuts_callback.hpp
@@ -15,20 +15,18 @@
 void cuts_callback(const auto& adj, auto f) {
   int n = sz(adj), q = 0, s = 0;
   vi tin(n), st(n);
-  auto dfs = [&](auto&& self, int v, int p) -> int {
-    int low = tin[v] = ++q;
-    for (int u : adj[v])
-      if (u != p) {
-        int siz = s, lu = 0;
-        low = min(low,
-          tin[u] ?: (lu = self(self, st[s++] = u, v)));
-        if (lu >= tin[v]) {
-          st[s++] = v;
-          f(vi(siz + all(st) - n + s));
-          s = siz;
-        }
+  auto dfs = [&](auto&& self, int v) -> int {
+    int l = tin[v] = ++q;
+    for (int u : adj[v]) {
+      int siz = s, lu = 0;
+      l = min(l, tin[u] ?: (lu = self(self, st[s++] = u)));
+      if (lu >= tin[v]) {
+        st[s++] = v;
+        f(vi(siz + all(st) - n + s));
+        s = siz;
       }
-    return low;
+    }
+    return l;
   };
-  rep(i, 0, n) if (!tin[i]) dfs(dfs, i, i);
+  rep(i, 0, n) if (!tin[i]) dfs(dfs, i);
 }
