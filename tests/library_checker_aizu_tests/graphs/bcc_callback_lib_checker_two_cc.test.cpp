574241f2694e233278c691876da68f3a253ad49e
Luke Videckis <lrvideckis@gmail.com>
2025-06-23 20:34:43 -0600

    different style of handling multiple edges for bridges

--- tests/library_checker_aizu_tests/graphs/bcc_callback_lib_checker_two_cc.test.cpp
+++ tests/library_checker_aizu_tests/graphs/bcc_callback_lib_checker_two_cc.test.cpp
@@ -8,32 +8,25 @@ int main() {
   int n, m;
   cin >> n >> m;
   vector<basic_string<int>> adj(n);
-  vector<basic_string<array<int, 2>>> adj_e_id(n);
-  for (int i = 0; i < n; i++) {
-    adj[i] += i;
-    adj_e_id[i] += {i, i};
-  }
+  for (int i = 0; i < n; i++) adj[i] += i;
+  vector<array<int, 2>> edges(m);
   for (int i = 0; i < m; i++) {
     int u, v;
     cin >> u >> v;
     adj[u] += v;
     adj[v] += u;
-    adj_e_id[u] += {v, n + i};
-    adj_e_id[v] += {u, n + i};
+    if (u > v) swap(u, v);
+    edges[i] = {u, v};
   }
   UF uf(n);
-  vector<bool> seen(n + m);
+  ranges::sort(edges);
+  for (int i = 1; i < m; i++)
+    if (edges[i - 1] == edges[i])
+      uf.join(edges[i][0], edges[i][1]);
   bcc_callback(adj, [&](const vi& nodes) {
     assert(sz(nodes) >= 2);
-    int cnt_edges = 0;
-    rep(i, 0, sz(nodes) - 1) for (auto [v, e_id] :
-      adj_e_id[nodes[i]]) if (!seen[e_id] &&
-      v != nodes[i]) {
-      seen[e_id] = 1;
-      cnt_edges++;
-    }
-    if (cnt_edges >= 2)
-      for (int v : nodes) uf.join(v, nodes[0]);
+    if (sz(nodes) == 2) return;
+    for (int v : nodes) uf.join(v, nodes[0]);
   });
   vector<basic_string<int>> two_edge_ccs(n);
   rep(i, 0, n) two_edge_ccs[uf.find(i)] += i;
