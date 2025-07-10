40cd17ab28f73c147ab537a1168df63af620ba2c
Luke Videckis <lrvideckis@gmail.com>
2025-07-10 12:06:06 -0600

    nit

--- library/data_structures/seg_tree_uncommon/wavelet_bit_vec.hpp
+++ library/data_structures/seg_tree_uncommon/wavelet_bit_vec.hpp
@@ -10,13 +10,14 @@ using ull = uint64_t;
 struct bit_vec {
   vector<pair<ull, int>> b;
   bit_vec(const vector<bool>& a): b(sz(a) / 64 + 1) {
-    rep(i, 0, sz(a)) b[i >> 6].first |= ull(a[i])
-      << (i & 63);
-    rep(i, 0, sz(b) - 1) b[i + 1].second =
-      popcount(b[i].first) + b[i].second;
+    rep(i, 0, sz(a)) {
+      auto& [x, y] = b[i >> 6];
+      x |= ull(a[i]) << (i & 63), y += a[i];
+    }
+    rep(i, 1, sz(b)) b[i].second += b[i - 1].second;
   }
   int cnt(int r) {
     auto [x, y] = b[r >> 6];
-    return y + popcount(x & ((1ULL << (r & 63)) - 1));
+    return y - popcount(x & -1ULL << (r & 63));
   }
 };
