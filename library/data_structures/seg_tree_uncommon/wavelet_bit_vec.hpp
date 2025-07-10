e79d2b8969610d74cdafaf1abc742281c69eb750
Luke Videckis <lrvideckis@gmail.com>
2025-07-10 11:29:14 -0600

    update docs for this

--- library/data_structures/seg_tree_uncommon/wavelet_bit_vec.hpp
+++ library/data_structures/seg_tree_uncommon/wavelet_bit_vec.hpp
@@ -1,18 +1,20 @@
 #pragma once
+//! @code
+//!   vector<bool> a(n);
+//!   bit_vec bv(a);
+//!   bv.cnt(r); // a[0] + a[1] + ... + a[r - 1]
+//! @endcode
+//! @time O(n + q)
+//! @space O(n / 64)
 #define ull uint64_t
 struct bit_vec {
   vector<pair<ull, int>> b;
-  //! @time O(n)
-  //! @space O(n / 64)
   bit_vec(const vector<bool>& a): b(sz(a) / 64 + 1) {
     rep(i, 0, sz(a)) b[i >> 6].first |= ull(a[i])
       << (i & 63);
     rep(i, 0, sz(b) - 1) b[i + 1].second =
       popcount(b[i].first) + b[i].second;
   }
-  //! @returns a[0] + a[1] + ... + a[r - 1]
-  //! @time O(1)
-  //! @space O(1)
   int cnt(int r) {
     auto [x, y] = b[r >> 6];
     return y + popcount(x & ((1ULL << (r & 63)) - 1));
