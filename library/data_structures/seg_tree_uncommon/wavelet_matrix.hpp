54ed8f1ab5150c7dccc5ea55a3ed03519fbbea23
Luke Videckis <lrvideckis@gmail.com>
2025-07-10 11:41:54 -0600

    Wavelet nit (#146)
    * nit to wavelet matrix
    
    * [auto-verifier] verify commit adaa1d2de83572702fde6834cce6d5bd64966de6
    
    * update docs
    
    * [auto-verifier] verify commit 60ca7c0177a8d7147a637d120c83527d3b16efb7
    
    * update docs for this
    
    * [auto-verifier] verify commit e79d2b8969610d74cdafaf1abc742281c69eb750
    
    * fix
    
    * fix
    
    * [auto-verifier] verify commit 5e317459902d24e4798d6aa3cd3f3f79640a022c
    
    * minor golf
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/data_structures/seg_tree_uncommon/wavelet_matrix.hpp
+++ library/data_structures/seg_tree_uncommon/wavelet_matrix.hpp
@@ -1,35 +1,34 @@
 #pragma once
+//! @code
+//!   vector<ull> a(n);
+//!   wavelet_matrix wm(a, 1e9); // requires a[i] <= 1e9
+//!   wm.kth(l, r, k); //(k+1)th smallest number in [l,r)
+//!   wm.kth(l, r, 0); //min in [l,r)
+//! @endcode
+//! @time O(n * log(max_val) + q * log(max_val))
+//! @space O(n * log(max_val) / 64)
 #include "wavelet_bit_vec.hpp"
 struct wavelet_matrix {
   int n;
   vector<bit_vec> bv;
-  //! Requires a[i] <= max_val
-  //! @time O(n * log(max_val))
-  //! @space O(n * log(max_val) / 64)
   wavelet_matrix(vector<ull> a, ull max_val):
     n(sz(a)), bv(bit_width(max_val), {{}}) {
-    vector<ull> nxt(n);
     for (int h = sz(bv); h--;) {
+      int i = 0;
       vector<bool> b(n);
-      rep(i, 0, n) b[i] = (a[i] >> h) & 1;
+      ranges::stable_partition(a,
+        [&](ull x) { return b[i++] = (~x >> h) & 1; });
       bv[h] = b;
-      array it{begin(nxt), begin(nxt) + bv[h].cnt0(n)};
-      rep(i, 0, n) * it[b[i]]++ = a[i];
-      swap(a, nxt);
     }
   }
-  //! (k+1)th smallest number in [l,r)
-  //! kth(l,r,0) returns the min
-  //! @time O(log(max_val))
-  //! @space O(1)
   ull kth(int l, int r, int k) {
     ll res = 0;
     for (int h = sz(bv); h--;) {
-      int l0 = bv[h].cnt0(l), r0 = bv[h].cnt0(r);
+      int l0 = bv[h].cnt(l), r0 = bv[h].cnt(r);
       if (k < r0 - l0) l = l0, r = r0;
       else
         k -= r0 - l0, res |= 1ULL << h,
-          l += bv[h].cnt0(n) - l0, r += bv[h].cnt0(n) - r0;
+          l += bv[h].cnt(n) - l0, r += bv[h].cnt(n) - r0;
     }
     return res;
   }
