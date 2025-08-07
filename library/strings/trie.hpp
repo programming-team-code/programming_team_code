a2cd22570adc65b626d75cbbb9d97330eb34211c
Luke Videckis <lrvideckis@gmail.com>
2025-08-06 19:25:08 -0600

    trie golf

--- library/strings/trie.hpp
+++ library/strings/trie.hpp
@@ -1,17 +1,13 @@
 #pragma once
 //! https://cp-algorithms.com/string/aho_corasick.html#construction-of-the-trie
-const int mn = 'A',
-          cnt_let = 26; // mn <= s[i] < mn + cnt_let
+const int mn = 'A';
 struct trie {
   struct node {
-    array<int, cnt_let> next;
-    int cnt_words = 0, par = -1;
-    char ch;
-    node(int par = -1, char ch = '#'): par(par), ch(ch) {
-      fill(all(next), -1);
-    }
+    array<int, 26> next;
+    bool end_of_word = 0;
+    node() { fill(all(next), -1); }
   };
-  vector<node> t;
+  deque<node> t;
   trie(): t(1) {}
   void insert(const string& s) {
     int v = 0;
@@ -19,11 +15,11 @@ struct trie {
       int u = ch - mn;
       if (t[v].next[u] == -1) {
         t[v].next[u] = sz(t);
-        t.emplace_back(v, ch);
+        t.emplace_back();
       }
       v = t[v].next[u];
     }
-    t[v].cnt_words++;
+    t[v].end_of_word = 1;
   }
   int find(const string& s) {
     int v = 0;
@@ -32,6 +28,6 @@ struct trie {
       if (t[v].next[u] == -1) return 0;
       v = t[v].next[u];
     }
-    return t[v].cnt_words;
+    return t[v].end_of_word;
   }
 };
