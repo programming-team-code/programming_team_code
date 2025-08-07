f0c9133599fe17a5146b488f2f1b8e52edcc6f90
Luke Videckis <lrvideckis@gmail.com>
2025-08-06 18:38:52 -0600

    nits to binary trie

--- tests/library_checker_aizu_tests/data_structures/binary_trie.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/binary_trie.test.cpp
@@ -1,34 +1,32 @@
 #define PROBLEM \
   "https://judge.yosupo.jp/problem/set_xor_min"
 #include "../template.hpp"
-#include "../../../library/data_structures/binary_trie/binary_trie.hpp"
+#include "../../../library/data_structures/binary_trie.hpp"
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   int q;
   cin >> q;
   {
-    binary_trie<int64_t> bt_ll;
-    assert(bt_ll.mx_bit == 62);
-    bt_ll.update(61238612412983LL, 5);
-    int cnt = bt_ll.count(61238612412983LL);
-    assert(cnt == 5);
-    cnt = bt_ll.count(54289162783746217LL);
-    assert(cnt == 0);
-    int64_t res = bt_ll.walk(54289162783746217LL);
-    assert(res == 61238612412983LL);
+    binary_trie bt;
+    bt.update(61238612412983LL, 5);
+    assert(
+      bt.walk(54289162783746217LL) == 61238612412983LL);
   }
-  binary_trie<int> bt_int;
-  assert(bt_int.mx_bit == 30);
+  binary_trie bt;
+  bt.update(0, 0);
   while (q--) {
     int type, x;
     cin >> type >> x;
     if (type == 0) {
-      if (bt_int.count(x) == 0) bt_int.update(x, 1);
+      if (
+        bt.t[bt.t[0].next[0]].siz == 0 || bt.walk(x) != x)
+        bt.update(x, 1);
     } else if (type == 1) {
-      if (bt_int.count(x) == 1) bt_int.update(x, -1);
+      if (bt.t[bt.t[0].next[0]].siz > 0 && bt.walk(x) == x)
+        bt.update(x, -1);
     } else {
       assert(type == 2);
-      int val = bt_int.walk(x);
+      int val = bt.walk(x);
       cout << (val ^ x) << '\n';
     }
   }
