85b56576bc1bc332030366118f8f86dadae3c929
Luke Videckis <lrvideckis@gmail.com>
2025-08-23 18:32:47 -0600

    another fix

--- library/strings/string_hash.hpp
+++ library/strings/string_hash.hpp
@@ -11,22 +11,22 @@
 //! @time O(n + q)
 //! @space O(n)
 using hsh = array<ll, 2>;
-constexpr hsh mod = {ll(1e9 + 7), ll(1e9 + 9)};
+constexpr hsh md = {ll(1e9 + 7), ll(1e9 + 9)};
 const ll base = 239017;
 struct str_hash {
   vector<hsh> ha, pw;
   str_hash(const auto& s): ha(sz(s) + 1), pw(ha) {
     pw[0] = {1, 1};
     rep(i, 0, sz(s)) rep(j, 0, 2) {
-      pw[i + 1][j] = pw[i][j] * base % mod[j];
-      ha[i + 1][j] = (ha[i][j] * base + s[i] + 1) % mod[j];
+      pw[i + 1][j] = pw[i][j] * base % md[j];
+      ha[i + 1][j] = (ha[i][j] * base + s[i] + 1) % md[j];
     }
   }
   hsh subarray(int l, int r) { // [l, r)
     hsh res;
     rep(j, 0, 2) {
-      res[j] = ha[r][j] - ha[l][j] * pw[r - l][j] % mod[j];
-      if (res[j] < 0) res[j] += mod[j];
+      res[j] = ha[r][j] - ha[l][j] * pw[r - l][j] % md[j];
+      if (res[j] < 0) res[j] += md[j];
     }
     return res;
   }
