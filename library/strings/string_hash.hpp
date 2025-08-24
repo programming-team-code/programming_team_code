3d021210ee9e46dc206864e571e3292e02c9a92d
Luke Videckis <lrvideckis@gmail.com>
2025-08-23 18:16:16 -0600

    fix

--- library/strings/string_hash.hpp
+++ library/strings/string_hash.hpp
@@ -1,14 +1,20 @@
 #pragma once
-//! https://codeforces.com/blog/entry/4898#comment-157965
+//! https://codeforces.com/blog/entry/4898
+//! https://codeforces.com/blog/entry/60442
+//! https://codeforces.com/blog/entry/129538
+//! p = probability of collision ~= 1/((1e9+7)*(1e9+9))
+//! probability of collision over q comparisons ~=
+//!  (1 - (1/p))^q ~= 1 - (1/p)^q
+//! checking if n hashes are pairwise distinct is the same
+//! as doing q = (n choose 2) ~= n^2 comparisons
 //! - use random base to avoid getting hacked on codeforces
-//! - work mod 1e9+7 if birthday paradox is not a problem
 //! @time O(n + q)
 //! @space O(n)
-using mint = array<ll, 2>;
-constexpr mint mod = {ll(1e9 + 7), ll(1e9 + 9)};
+using hsh = array<ll, 2>;
+constexpr hsh mod = {ll(1e9 + 7), ll(1e9 + 9)};
 const ll base = 239017;
 struct str_hash {
-  vector<mint> ha, pw;
+  vector<hsh> ha, pw;
   str_hash(const auto& s): ha(sz(s) + 1), pw(ha) {
     pw[0] = {1, 1};
     rep(i, 0, sz(s)) rep(j, 0, 2) {
@@ -16,8 +22,8 @@ struct str_hash {
       ha[i + 1][j] = (ha[i][j] * base + s[i] + 1) % mod[j];
     }
   }
-  mint subarray(int l, int r) { // [l, r)
-    mint res;
+  hsh subarray(int l, int r) { // [l, r)
+    hsh res;
     rep(j, 0, 2) {
       res[j] = ha[r][j] - ha[l][j] * pw[r - l][j] % mod[j];
       if (res[j] < 0) res[j] += mod[j];
