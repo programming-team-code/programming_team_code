0227c7d1e09724a7871c7419265f34258fe6d6a1
Luke Videckis <lrvideckis@gmail.com>
2025-08-30 00:58:07 -0600

    Update shallowest_decomp_tree.hpp

--- library/trees/shallowest_decomp_tree.hpp
+++ library/trees/shallowest_decomp_tree.hpp
@@ -21,7 +21,7 @@ void shallowest(auto& adj, auto f) {
     return dp;
   };
   dfs(dfs, 0, 0);
-  for (const vi& vec : order | views::reverse)
+  for (vi vec : order | views::reverse)
     for (int v : vec) {
       f(v);
       for (int u : adj[v])
