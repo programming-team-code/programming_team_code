32fc223db30f1744e0aa21e17e746afba7ce5c9e
Luke Videckis <lrvideckis@gmail.com>
2025-09-07 15:15:17 -0600

    add single function for mod division

--- library/math/mod_int_division.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-mint operator/(mint b) {
-  int m = mod, u = 1, v = 0;
-  while (m)
-    u = exchange(v, u - b.x / m * v),
-    b.x = exchange(m, b.x % m);
-  assert(b.x == 1);
-  return *this * u;
-}
