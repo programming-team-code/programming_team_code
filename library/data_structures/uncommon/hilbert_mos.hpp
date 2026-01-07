68f83939ad0b7867a1343664038f1bcea73b893f
Luke Videckis <lrvideckis@gmail.com>
2026-01-07 15:54:16 -0600

    fixes

--- library/data_structures/uncommon/hilbert_mos.hpp
+++ library/data_structures/uncommon/hilbert_mos.hpp
@@ -1,12 +1,12 @@
 #pragma once
 //! https://codeforces.com/blog/entry/61203?#comment-1064868
 //! @code
-//!   vector<pii> queries;
-//!   ranges::sort(queries, {}, [&](pii& x) {
-//!     return hilbert(x.first, x.second);
+//!   vector<array<int, 3>> queries;
+//!   ranges::sort(queries, {}, [&](array<int, 3>& x) {
+//!     return hilbert(x[0], x[1]);
 //!   });
 //! @endcode
-//! @time O(log(n))
+//! @time O(log(max(x, y)))
 //! @space O(1)
 ll hilbert(int x, int y) {
   ll d = 0, mx = 1;
