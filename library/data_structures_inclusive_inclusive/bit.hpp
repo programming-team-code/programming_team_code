4f2e9e7683b3b2367b3a4598cfc3b7338f98cf05
Luke Videckis <lrvideckis@gmail.com>
2026-01-17 14:16:45 -0700

    more reorg

--- library/data_structures_inclusive_inclusive/bit.hpp
+++ library/data_structures_inclusive_inclusive/bit.hpp
@@ -1,9 +1,11 @@
 #pragma once
 //! @time O(n + q log n)
 //! @space O(n)
-struct bit_inc {
+// NOLINTNEXTLINE(readability-identifier-naming)
+struct BIT {
   vector<ll> s;
-  bit_inc(int n): s(n) {}
+  BIT(int n): s(n) {}
+#include "bit_uncommon/vector_constructor.hpp"
   void update(int i, ll d) {
     for (; i < sz(s); i |= i + 1) s[i] += d;
   }
