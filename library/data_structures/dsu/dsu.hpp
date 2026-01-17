c1efed1a6339944f7b8a3ee53c73163a7959e82c
Luke Videckis <lrvideckis@gmail.com>
2026-01-17 12:26:02 -0700

    rename

--- library/data_structures/dsu/dsu.hpp
+++ library/data_structures/dsu/dsu.hpp
@@ -3,10 +3,10 @@
 struct DSU {
   vi p;
   DSU(int n): p(n, -1) {}
-  int size(int x) { return -p[go(x)]; }
-  int go(int x) { return p[x] < 0 ? x : p[x] = go(p[x]); }
+  int size(int x) { return -p[f(x)]; }
+  int f(int x) { return p[x] < 0 ? x : p[x] = f(p[x]); }
   bool join(int a, int b) {
-    if ((a = go(a)) == (b = go(b))) return 0;
+    if ((a = f(a)) == (b = f(b))) return 0;
     if (p[a] > p[b]) swap(a, b);
     return p[a] += p[b], p[b] = a, 1;
   }
