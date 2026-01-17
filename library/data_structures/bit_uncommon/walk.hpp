040cc2d09d115892ce03fe40919ff78d5b09b2a4
Luke Videckis <lrvideckis@gmail.com>
2026-01-17 12:18:24 -0700

    update docs, I find this way easier

--- library/data_structures/bit_uncommon/walk.hpp
+++ library/data_structures/bit_uncommon/walk.hpp
@@ -1,10 +1,11 @@
-//! Requires s[i] >= 0
-//! max r such that sum of [0,r) < sum, or -1
+//! Requires sum of [i,i] >= 0
+//! Returns min r st sum of [0,r] >= sum
+//! Returns n if sum of [0,n-1] < sum
 int walk(ll sum) {
   if (sum <= 0) return -1;
   int r = 0;
-  for (int pw = bit_floor(size(s)); pw; pw >>= 1)
-    if (r + pw <= sz(s) && s[r + pw - 1] < sum)
-      sum -= s[(r += pw) - 1];
+  for (int i = bit_floor(size(s)); i; i /= 2)
+    if (r + i <= sz(s) && s[r + i - 1] < sum)
+      sum -= s[(r += i) - 1];
   return r;
 }
