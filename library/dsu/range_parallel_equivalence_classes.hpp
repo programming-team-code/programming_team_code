665fb54bb2fb4b5f5bfc78790425ec1a095fd6eb
Luke Videckis <lrvideckis@gmail.com>
2026-01-17 15:12:43 -0700

    more fixes

--- library/dsu/range_parallel_equivalence_classes.hpp
+++ library/dsu/range_parallel_equivalence_classes.hpp
@@ -1,17 +1,17 @@
 #pragma once
-#include "../../../kactl/content/data-structures/UnionFind.h"
+#include "dsu.hpp"
 //! Given triplets (l1,l2,len); joins (l1,l2),
 //!   (l1+1,l2+1), ..., (l1+len-1,l2+len-1)
 //! @time O((n + q) * \alpha(n))
 //! @space O(n + q)
-UF get_rp_dsu(const vector<array<int, 3>>& rests, int n) {
+DSU get_rp_dsu(const vector<array<int, 3>>& rests, int n) {
   vector<vector<pii>> rests_by_len(n + 1);
   for (auto [l1, l2, len] : rests)
     rests_by_len[len].emplace_back(l1, l2);
-  UF uf(n);
+  DSU dsu(n);
   for (int len = n; len > 0; len--)
     for (auto [l1, l2] : rests_by_len[len])
-      if (uf.join(l1, l2))
+      if (dsu.join(l1, l2))
         rests_by_len[len - 1].emplace_back(l1 + 1, l2 + 1);
-  return uf;
+  return dsu;
 }
--- tests/library_checker_aizu_tests/handmade_tests/seg_tree_find_small.test.cpp
+++ tests/library_checker_aizu_tests/handmade_tests/seg_tree_find_small.test.cpp
@@ -2,7 +2,7 @@
   "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
 #include "../template.hpp"
 #include "../../../library/contest/random.hpp"
-#include "../../../library/data_structures/lazy_seg_tree.hpp"
+#include "../../../library/data_structures_[l,r)/lazy_seg_tree.hpp"
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   for (int iters = 0; iters < 100; ++iters) {
