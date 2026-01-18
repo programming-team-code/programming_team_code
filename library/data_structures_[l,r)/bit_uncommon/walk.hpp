54118a25bd4a5a1fd31a5b155def9cc39f155813
Luke Videckis <lrvideckis@gmail.com>
2026-01-18 02:48:00 -0700

    Fix typo in comment for walk function

--- library/data_structures_[l,r)/bit_uncommon/walk.hpp
+++ library/data_structures_[l,r)/bit_uncommon/walk.hpp
@@ -1,5 +1,5 @@
 //! Requires sum of [i,i] >= 0
-//! Returns min r st sum of [0,r] >= sum
+//! Returns min r s.t. sum of [0,r] >= sum
 //! Returns n if sum of [0,n-1] < sum
 int walk(ll sum) {
   if (sum <= 0) return -1;
