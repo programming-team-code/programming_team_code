20c104b1c6a60e678fd61e96ccd7c172a4efde03
Luke Videckis <lrvideckis@gmail.com>
2026-01-19 08:48:20 -0700

    golf

--- library/math/matrix_related/xor_basis_unordered.hpp
+++ library/math/matrix_related/xor_basis_unordered.hpp
@@ -1,22 +1,18 @@
 #pragma once
 //! https://codeforces.com/blog/entry/68953
 //! https://codeforces.com/blog/entry/100066?#comment-887897
-//! @code
-//!   basis<int> b1;
-//!   basis<ll> b2;
-//! @endcode
 //! b.shrink(v) == b.shrink(b.shrink(v))
-//! for x in b.b: (bit_floor(x)&b.shrink(v))==0
-//! for 0<=i<j<sz(b.b): (bit_floor(b[i])&b[j])==0
-//! @time O(32) or O(64)
-//! @space O(32) or O(64)
-template<class T> struct basis {
-  vector<T> b;
-  T shrink(T v) {
-    for (T x : b) v = min(v, v ^ x);
+//! for x in b: (bit_floor(x)&b.shrink(v))==0
+//! for 0<=i<j<sz(b): (bit_floor(b[i])&b[j])==0
+//! @time O(32)
+//! @space O(32)
+struct basis {
+  vi b;
+  int shrink(int v) {
+    for (int x : b) v = min(v, v ^ x);
     return v;
   }
-  bool insert(T v) {
+  bool insert(int v) {
     return (v = shrink(v)) ? b.push_back(v), 1 : 0;
   }
 };
