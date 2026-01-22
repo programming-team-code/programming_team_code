78ccda098b9a514f40dc96b3671c47e0b2bfc6c8
Luke Videckis <lrvideckis@gmail.com>
2026-01-21 23:18:17 -0700

    Format constructor for better readability

--- library/data_structures_[l,r]/seg_tree.hpp
+++ library/data_structures_[l,r]/seg_tree.hpp
@@ -19,7 +19,8 @@ template<class T, class F> struct tree {
   int n;
   F op;
   vector<T> s;
-  tree(int n, T unit, F op): n(n), op(op), s(2 * n, unit) {}
+  tree(int n, T unit, F op):
+    n(n), op(op), s(2 * n, unit) {}
   void update(int i, T val) {
     for (s[i += n] = val; i /= 2;)
       s[i] = op(s[2 * i], s[2 * i + 1]);
