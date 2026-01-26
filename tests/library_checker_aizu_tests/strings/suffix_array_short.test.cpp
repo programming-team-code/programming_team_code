711cb9b1b87b2589cb24ee0a01714b46711ec12f
Luke Videckis <lrvideckis@gmail.com>
2026-01-25 18:47:22 -0700

    Update suffix array test to include LCP verification

--- tests/library_checker_aizu_tests/strings/suffix_array_short.test.cpp
+++ tests/library_checker_aizu_tests/strings/suffix_array_short.test.cpp
@@ -9,11 +9,22 @@ int main() {
   cin.tie(0)->sync_with_stdio(0);
   string s;
   cin >> s;
-  auto [sa, sa_inv, _] = sa_short(vi(all(s)));
-  for (int i = 0; i < sz(s); i++) {
+  auto [sa, sa_inv, lcp] = sa_short(vi(all(s)));
+  rep (i, 0, sz(s)) {
     assert(sa[sa_inv[i]] == i);
     assert(sa_inv[sa[i]] == i);
   }
+  vi lcp_kasai(sz(s) - 1);
+  int sz = 0;
+  rep(i, 0, n) {
+    if (sz > 0) sz--;
+    if (sa_inv[i] == 0) continue;
+    for (int j = sa[sa_inv[i] - 1];
+      max(i, j) + sz < n && s[i + sz] == s[j + sz];)
+      sz++;
+    lcp_kasai[sa_inv[i] - 1] = sz;
+  }
+  assert(lcp == lcp_kasai);
   for (int val : sa) cout << val << " ";
   cout << '\n';
 }
