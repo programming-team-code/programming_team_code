82db04b4161343a7822eb7c2a64f55faaa23df91
Luke Videckis <lrvideckis@gmail.com>
2026-01-27 11:07:54 -0700

    fixes

--- tests/scripts/grep_clangformat_cppcheck.sh
+++ tests/scripts/grep_clangformat_cppcheck.sh
@@ -16,9 +16,6 @@ grep --extended-regexp "template\s?<typename" --recursive ../library/ && exit 1
 echo "check 1 instead of true"
 grep "true" --recursive ../library/ && exit 1
 
-echo "check 0 instead of false"
-grep "false" --recursive ../library/ && exit 1
-
 echo "check ll instead of long long or int64_t"
 grep "long long" --recursive ../library/ && exit 1
 grep "int64_t" --recursive ../library/**/*.hpp | grep "uint64_t" --invert-match && exit 1
