f1f13f2c151d47d5f8c01f740d0c80484331fd7e
Luke Videckis <lrvideckis@gmail.com>
2026-02-05 13:02:29 -0700

    nit

--- library/data_structures_[l,r]/seg_tree_midpoint.hpp
+++ library/data_structures_[l,r]/seg_tree_midpoint.hpp
@@ -5,6 +5,6 @@
 //!   // [tl,tr] splits into [tl,tm] and [tm+1,tr]
 //! @endcode
 int split(int tl, int tr) {
-  int pw2 = 1 << __lg(tr - tl + 1);
+  int pw2 = bit_floor(tr - tl + 1u);
   return min(tl + pw2 - 1, tr - pw2 / 2);
 }
