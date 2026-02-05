182caaf26b0153fcdba7d781495b1f52f09ba458
Luke Videckis <lrvideckis@gmail.com>
2026-02-05 16:06:35 -0700

    previous code is RTE when n=0

--- library/data_structures_[l,r]/seg_tree_uncommon/init.hpp
+++ library/data_structures_[l,r]/seg_tree_uncommon/init.hpp
@@ -2,5 +2,6 @@
 tree(const vector<T>& a, F op):
   n(sz(a)), op(op), s(2 * n) {
   rep(i, 0, n) s[i + n] = a[i];
-  for (int i = n; --i;) s[i] = op(s[2 * i], s[2 * i + 1]);
+  for (int i = n - 1; i >= 1; i--)
+    s[i] = op(s[2 * i], s[2 * i + 1]);
 }
