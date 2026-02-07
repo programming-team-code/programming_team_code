4c88f6150dfd7f9f69a45d4e03618252340b8d0f
Luke Videckis <lrvideckis@gmail.com>
2026-02-07 02:31:09 -0700

    Enhance walk function with optimized logic
    Refactor walk function to improve performance and clarity.

--- library/data_structures_[l,r]/seg_tree_uncommon/walk.hpp
+++ library/data_structures_[l,r]/seg_tree_uncommon/walk.hpp
@@ -1,4 +1,10 @@
+int go(int l, int r) {
+//return __lg(min(l & -l, r - l + 1));
+//return __lg(min((l + n) & -(l + n), r - l + 1));
+  return min(__builtin_ctz(l + n), __lg(r - l + 1));
+}
 int walk(int l, int r, const auto& f) {
+  /*
   for (l += n, r += n; l <= r;)
     if (int u = nxt(l, r); f(s[u])) {
       while (u < n)
@@ -6,5 +12,11 @@ int walk(int l, int r, const auto& f) {
         else (u *= 2)++;
       return u - n;
     }
+  */
+  while (l <= r) {
+    int u = l + n, x = __lg(min(u & -u, r - l + 1));
+    if (f(s[u >> x])) r = l + (1 << x) - 1;
+    else l += 1 << x;
+  }
   return -1;
 }
