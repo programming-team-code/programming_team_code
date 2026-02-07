3beb2d193a58f8d1dc1d6fde6e03cf5600d88b7a
Luke Videckis <lrvideckis@gmail.com>
2026-02-07 02:34:21 -0700

    Fix boundary condition in segment tree walk

--- library/data_structures_[l,r]/seg_tree_uncommon/walk.hpp
+++ library/data_structures_[l,r]/seg_tree_uncommon/walk.hpp
@@ -15,7 +15,7 @@ int walk(int l, int r, const auto& f) {
   */
   while (l <= r) {
     int u = l + n, x = __lg(min(u & -u, r - l + 1));
-    if (f(s[u >> x])) r = l + (1 << x) - 1;
+    if (f(s[u >> x])) r = l + (1 << x) - 2;
     else l += 1 << x;
   }
   return -1;
