5743f65eb05655b313f4d4d52d816d16839ec4de
Luke Videckis <lrvideckis@gmail.com>
2026-02-07 17:12:53 -0700

    Reverse conditions in walk function logic

--- library/data_structures_[l,r]/seg_tree_uncommon/walk.hpp
+++ library/data_structures_[l,r]/seg_tree_uncommon/walk.hpp
@@ -1,8 +1,8 @@
 int walk(int l, int r, const auto& f) {
   while (l <= r) {
     int u = l + n, x = __lg(min(u & -u, r - l + 1));
-    if (f(s[u >> x])) r = l + (1 << x) - 2;
-    else l += 1 << x;
+    if (f(s[u >> x])) l += 1 << x;
+    else r = l + (1 << x) - 2;
   }
   return l;
 }
