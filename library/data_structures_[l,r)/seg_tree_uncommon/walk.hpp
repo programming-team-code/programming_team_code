f4ea8f07217e27a089c2e19a2932b0aae109c32c
Luke Videckis <lrvideckis@gmail.com>
2026-02-13 17:15:48 -0700

    Refactor walk function to initialize x inside loop

--- library/data_structures_[l,r)/seg_tree_uncommon/walk.hpp
+++ library/data_structures_[l,r)/seg_tree_uncommon/walk.hpp
@@ -1,6 +1,5 @@
 int walk(int l, int r, const auto& f) {
-  T x = unit;
-  while (l < r) {
+  while (T x = unit; l < r) {
     int u = l + n, v = __lg(min(u & -u, r - l));
     if (T y = op(x, s[u >> v]); f(y)) x = y, l += 1 << v;
     else r = l + (1 << v) - 1;
