1db1d299230f834029452390d68d72c8dab7ed9f
Luke Videckis <lrvideckis@gmail.com>
2026-02-13 17:54:07 -0700

    Update walk.hpp

--- library/data_structures_[l,r)/seg_tree_uncommon/walk.hpp
+++ library/data_structures_[l,r)/seg_tree_uncommon/walk.hpp
@@ -1,5 +1,5 @@
 int walk(int l, int r, const auto& f) {
-  while (T x = unit; l < r) {
+  for (T x = unit; l < r;) {
     int u = l + n, v = __lg(min(u & -u, r - l));
     if (T y = op(x, s[u >> v]); f(y)) x = y, l += 1 << v;
     else r = l + (1 << v) - 1;
