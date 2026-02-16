eb9f7c91435288501fab5fbf7e73924a86c5538d
Luke Videckis <lrvideckis@gmail.com>
2026-02-16 13:02:48 -0700

    golf

--- library/data_structures_[l,r]/seg_tree_uncommon/walk.hpp
+++ library/data_structures_[l,r]/seg_tree_uncommon/walk.hpp
@@ -1,10 +1,9 @@
 int walk(int l, int r, const auto& f) {
-  l += n, r += n;
-  if (T x = s[l]; f(x))
+  if (T x = s[l + n]; f(x))
     for (l++; l <= r;) {
-      int v = __lg(min(l & -l, r - l + 1));
-      if (T y = op(x, s[l >> v]); f(y)) l += 1 << v, x = y;
+      int u = l + n, v = __lg(min(u & -u, r - l + 1));
+      if (T y = op(x, s[u >> v]); f(y)) l += 1 << v, x = y;
       else r = l + (1 << v) - 2;
     }
-  return l - n;
+  return l;
 }
