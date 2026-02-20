c18dd348094fa4c161b4bf440ebc081d432b7b41
Luke Videckis <lrvideckis@gmail.com>
2026-02-19 18:06:52 -0700

    even more golf

--- library/data_structures_[l,r)/bit_uncommon/kd_bit.hpp
+++ library/data_structures_[l,r)/bit_uncommon/kd_bit.hpp
@@ -14,12 +14,11 @@
 // NOLINTNEXTLINE(readability-identifier-naming)
 template<int K> struct KD_BIT {
   vector<KD_BIT<K - 1>> s;
-  template<class... A>
-  KD_BIT(int n, A... a): s(n, KD_BIT<K - 1>(a...)) {}
-  template<class... A> void update(int i, A... a) {
+  KD_BIT(int n, auto... a): s(n, KD_BIT<K - 1>(a...)) {}
+  void update(int i, auto... a) {
     for (; i < sz(s); i |= i + 1) s[i].update(a...);
   }
-  template<class... A> ll query(int l, int r, A... a) {
+  ll query(int l, int r, auto... a) {
     ll ans = 0;
     for (; l < r; r &= r - 1) ans += s[r - 1].query(a...);
     for (; r < l; l &= l - 1) ans -= s[l - 1].query(a...);
