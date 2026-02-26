47a8c8c32ddb7fcbec2c9b56537e0c15da65c986
Luke Videckis <lrvideckis@gmail.com>
2026-02-26 13:29:50 -0700

    euler walk first draft (#186)
    * euler walk first draft
    
    * [auto-verifier] verify commit fa84857a3b6b1c6db76979f88e29f2f4d2d41f61
    
    * nits
    
    * [auto-verifier] verify commit f28be0d481799b1cebcfa60ce9a0d910046cda88
    
    * ni
    
    * [auto-verifier] verify commit 36770a6bce59c44e6e0eb48e10a8616dd343d08e
    
    * add docs now
    
    * [auto-verifier] verify commit f920eba22e66250a118c74474b61ea95a0ec974b
    
    * renaming
    
    * update
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ library/graphs/euler_path.hpp
@@ -0,0 +1,28 @@
+#pragma once
+//! @code
+//!   vector<basic_string<array<int, 2>>> adj(n);
+//!   rep(i, 0, m) {
+//!     int u, v;
+//!     cin >> u >> v;
+//!     u--, v--;
+//!     adj[u] += {v, i};
+//!   }
+//!   vector<pii> path = euler_path(adj, m, source);
+//! @endcode
+//! @time O(n + m)
+//! @space O(n + m)
+vector<pii> euler_path(auto& adj, int m, int s) {
+  vi vis(m);
+  vector<pii> path;
+  auto dfs = [&](auto&& self, int u, int eu) -> void {
+    while (!empty(adj[u])) {
+      auto [v, ev] = adj[u].back();
+      adj[u].pop_back();
+      if (!vis[ev]) vis[ev] = 1, self(self, v, ev);
+    }
+    path.emplace_back(u, eu);
+  };
+  dfs(dfs, s, -1);
+  ranges::reverse(path);
+  return path;
+}
