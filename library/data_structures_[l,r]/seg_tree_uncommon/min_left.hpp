683e7f133a820d1b522e597b8515141a23050ee7
Luke Videckis <lrvideckis@gmail.com>
2026-03-01 18:53:04 -0700

    update test

--- library/data_structures_[l,r]/seg_tree_uncommon/min_left.hpp
+++ library/data_structures_[l,r]/seg_tree_uncommon/min_left.hpp
@@ -1,7 +1,6 @@
 int min_left(int l, int r, const auto& f) {
   if (T x = s[r + n]; f(x))
     for (r--; l <= r;) {
-      cout << "l,r:" << l << ' ' << r << endl; // 0 1
       int u = r + 1 + n, v = __lg(min(u & -u, r - l + 1));
       if (T y = op(s[(u - 1) >> v], x); f(y))
         r -= 1 << v, x = y;
