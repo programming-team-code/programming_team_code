05e100d83b3db7779f4573cce29167ffe17aa977
Luke Videckis <lrvideckis@gmail.com>
2026-03-06 12:07:47 -0700

    add docs now

--- library/convolution/xor_convolution.hpp
+++ library/convolution/xor_convolution.hpp
@@ -1,9 +1,16 @@
 #pragma once
 #include "../math/mod_division.hpp"
+//! https://codeforces.com/blog/entry/127823
+//! @code
+//!   vi c = xor_conv(a, b);
+//!   // must have sz(a) == sz(b) == a power of 2
+//!   // c[k] = sum of a[i]*b[j] where i^j==k
+//! @endcode
+//! @time O(n log n)
+//! @space O(n)
 void fwht(int n, vi& a) {
-  for (int i = 1; i != n; i <<= 1)
-    for (int j = 0; j != n; j += i << 1)
-      for (int k = 0; k != i; k++) {
+  for (int i = 1; i < n; i *= 2)
+    for (int j = 0; j < n; j += 2 * i) rep(k, 0, i) {
         int x = a[j + k], y = a[i + j + k];
         a[j + k] = (x + y) % mod;
         a[i + j + k] = (x - y + mod) % mod;
