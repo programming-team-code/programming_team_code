08c254ebf9eb9950adf7a5ad564bd9a7cf20c861
Luke Videckis <lrvideckis@gmail.com>
2026-03-08 13:49:14 -0600

    update docs again

--- library/math/matrix_related/xor_basis_ordered.hpp
+++ library/math/matrix_related/xor_basis_ordered.hpp
@@ -4,8 +4,9 @@
 //! @code
 //!   const int B = 30;
 //!   xor_basis<B> b;
-//!   bool in_span = b.shrink(x) >= 0;
-//!   b.insert(x);
+//!   bitset<B> v;
+//!   bool in_span = b.shrink(v) >= 0;
+//!   b.insert(v);
 //!   bitset<B> k = 5; // 0 <= k < (1 << npivot)
 //!   bitset<B> kth = b.walk(k);
 //!   // (k+1)-th smallest vector in span
