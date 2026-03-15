f20db6863b76423846cfdb26bacd06ba0c1cdb72
Luke Videckis <lrvideckis@gmail.com>
2026-03-15 12:46:01 -0600

    now inc-exc changes

--- library/data_structures_[l,r)/seg_tree_uncommon/max_right.hpp
+++ library/data_structures_[l,r)/seg_tree_uncommon/max_right.hpp
@@ -1,8 +1,8 @@
-int max_right(int l, int r, const auto& f) {
+void max_right(int l, int r, const auto& f) {
   for (T x = unit; l < r;) {
-    int u = l + n, v = __lg(min(u & -u, r - l));
-    if (T y = op(x, s[u >> v]); f(y)) l += 1 << v, x = y;
-    else r = l + (1 << v) - 1;
+    int u = l + n, v = __lg(min(u & -u, r - l)),
+        m = l + (1 << v);
+    if (T y = op(x, s[u >> v]); f(m, y)) l = m, x = y;
+    else r = m - 1;
   }
-  return l;
 }
