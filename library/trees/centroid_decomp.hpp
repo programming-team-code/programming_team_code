6a3c6c6cf8c97dc3387e473c793c5eea405d724e
Luke Videckis <lrvideckis@gmail.com>
2026-03-16 13:52:41 -0600

    update centroid

--- library/trees/centroid_decomp.hpp
+++ library/trees/centroid_decomp.hpp
@@ -6,21 +6,15 @@
 //! @endcode
 //! @time O(n log n)
 //! @space O(n)
-template<class F, class G> struct centroid {
-  G adj;
-  F f;
-  vi siz;
-  centroid(const G& adj, F f):
-    adj(adj), f(f), siz(sz(adj), -1) {
-    dfs(0, -1);
-  }
-  void calc_sz(int u, int p) {
+void centroid(auto& adj, const auto& f) {
+  vi siz(sz(adj));
+  auto calc_sz = [&](auto&& self, int u, int p) -> void {
     siz[u] = 1;
     for (int v : adj[u])
-      if (v != p) calc_sz(v, u), siz[u] += siz[v];
-  }
-  void dfs(int u, int p) {
-    calc_sz(u, -1);
+      if (v != p) self(self, v, u), siz[u] += siz[v];
+  };
+  auto dfs = [&](auto&& self, int u, int p) -> void {
+    calc_sz(calc_sz, u, -1);
     for (int w = -1, sz_root = siz[u];;) {
       auto big_ch = ranges::find_if(adj[u], [&](int v) {
         return v != w && 2 * siz[v] > sz_root;
@@ -28,11 +22,12 @@ template<class F, class G> struct centroid {
       if (big_ch == end(adj[u])) break;
       w = u, u = *big_ch;
     }
-    f(adj, u, p);
+    f(u, p);
     for (int v : adj[u]) {
       iter_swap(ranges::find(adj[v], u), rbegin(adj[v]));
       adj[v].pop_back();
-      dfs(v, u);
+      self(self, v, u);
     }
-  }
-};
+  };
+  dfs(dfs, 0, -1);
+}
