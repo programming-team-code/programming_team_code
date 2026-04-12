48ad957a400e7a806c5bcaa75851ba93fde7d6f6
Luke Videckis <lrvideckis@gmail.com>
2026-04-12 13:36:24 -0600

    name nit

--- library/trees/centroid_decomp.hpp
+++ library/trees/centroid_decomp.hpp
@@ -7,17 +7,17 @@
 //! @space O(n)
 void centroid(auto& g, auto f) {
   vi siz(sz(g));
-  auto cnt = [&](auto&& cnt, int u, int p, int n) -> int {
+  auto ctd = [&](auto&& ctd, int u, int p, int n) -> int {
     siz[u] = 1;
     for (int v : g[u])
       if (v != p) {
-        if (int c = cnt(cnt, v, u, n); c != -1) return c;
+        if (int c = ctd(ctd, v, u, n); c != -1) return c;
         siz[u] += siz[v];
       }
     return 2 * siz[u] >= n ? siz[p] = n - siz[u], u : -1;
   };
   auto dfs = [&](auto&& dfs, int u, int p, int n) -> void {
-    f(u = cnt(cnt, u, u, n), p);
+    f(u = ctd(ctd, u, u, n), p);
     for (int v : g[u])
       erase(g[v], u), dfs(dfs, v, u, siz[v]);
   };
--- library/trees/edge_cd.hpp
+++ library/trees/edge_cd.hpp
@@ -15,11 +15,11 @@
 //! @space O(n)
 template<class G> void edge_cd(vector<G>& g, auto f) {
   vi siz(sz(g));
-  auto cnt = [&](auto&& cnt, int u, int p, int m) -> int {
+  auto ctd = [&](auto&& ctd, int u, int p, int m) -> int {
     siz[u] = 1;
     for (int v : g[u])
       if (v != p) {
-        if (int c = cnt(cnt, v, u, m); c != -1) return c;
+        if (int c = ctd(ctd, v, u, m); c != -1) return c;
         siz[u] += siz[v];
       }
     return 2 * siz[u] > m ? siz[p] = m + 1 - siz[u],
@@ -27,7 +27,7 @@ template<class G> void edge_cd(vector<G>& g, auto f) {
   };
   auto dfs = [&](auto&& dfs, int u, int m) -> void {
     if (m < 2) return;
-    u = cnt(cnt, u, u, m);
+    u = ctd(ctd, u, u, m);
     int sum = 0;
     auto it = partition(all(g[u]), [&](int v) {
       ll x = sum + siz[v];
