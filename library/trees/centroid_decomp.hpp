02ae4c628d2af3e90cb701c74551e620e685dc7c
Luke Videckis <lrvideckis@gmail.com>
2026-04-26 01:58:54 -0600

    another golf

--- library/trees/centroid_decomp.hpp
+++ library/trees/centroid_decomp.hpp
@@ -6,18 +6,18 @@
 //! @time O(n log n)
 //! @space O(n)
 vi cd(auto& g, auto f) {
-  vi p(sz(g), -1), siz(sz(g), sz(g));
+  vi p(sz(g), -1), s(sz(g), sz(g));
   auto ctd = [&](auto ctd, int u, int p, int n) -> int {
-    siz[u] = 1;
+    s[u] = 1;
     for (int v : g[u])
       if (v != p) {
         if (int c = ctd(ctd, v, u, n); c != -1) return c;
-        siz[u] += siz[v];
+        s[u] += s[v];
       }
-    return 2 * siz[u] >= n ? siz[p] = n - siz[u], u : -1;
+    return 2 * s[u] >= n ? s[p] = n - s[u], u : -1;
   };
   auto dfs = [&](auto dfs, int u) -> int {
-    f(u = ctd(ctd, u, u, siz[u]));
+    f(u = ctd(ctd, u, u, s[u]));
     for (int v : g[u]) erase(g[v], u), p[dfs(dfs, v)] = u;
     return u;
   };
