4e346258f2781712f7f165cd21f6a14af4f86847
Luke Videckis <lrvideckis@gmail.com>
2026-05-01 11:34:51 -0600

    add documentation

--- library/graphs/min_vertex_cover.hpp
+++ library/graphs/min_vertex_cover.hpp
@@ -1,4 +1,16 @@
 #include "../../kactl/content/graph/HopcroftKarp.h"
+//! @code
+//!   int l_sz, r_sz;
+//!   vector<vi> g(l_sz);
+//!   g[u].push_back(v); // 0 <= u < l_sz, 0 <= v < r_sz
+//!   vi r(r_sz, -1);
+//!   int match_sz = hopcroftKarp(g, r);
+//!   // r[v] != -1 iff edge r[v] <-> v in matching
+//!   auto [mvc_l, mvc_r] = cover(g, r);
+//!   // mvc_l[u] == 1 iff u in mvc
+//! @endcode
+//! @time O(n + q * log n)
+//! @space O(n * \alpha(n))
 pair<vi, vi> cover(const vector<vi>& g, vi& r) {
   int n = sz(g), t = 0;
   vi cl(n), cr(sz(r)), q(n);
